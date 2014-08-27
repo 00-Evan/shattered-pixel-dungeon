fba96fe4ccabffb7d46ff42371e9b852fbea6f50
Evan Debenham <Evan@ShatteredPixel.com>
2014-08-27 17:00:08 -0400

    V0.2.0: created Chalice of Blood class

---
+++ src/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/ChaliceOfBlood.java
@@ -0,0 +1,18 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.artifacts;
+
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+/**
+ * Created by debenhame on 27/08/2014.
+ */
+public class ChaliceOfBlood extends Artifact {
+
+    {
+        name = "Chalice of Blood";
+        image = ItemSpriteSheet.ARTIFACT_CHALICE;
+        level = 0;
+        //charge & chargecap are unused
+    }
+
+
+}
