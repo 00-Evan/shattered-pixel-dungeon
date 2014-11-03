eeca1cb7dbc411c875ff313ee5e9d1c32de632c6
Evan Debenham <Evan.SHPX@gmail.com>
2014-11-02 19:39:16 -0500

    v0.2.2: fixed a bug with the ankh

--- src/com/shatteredpixel/shatteredpixeldungeon/items/Ankh.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/items/Ankh.java
@@ -68,7 +68,7 @@ public class Ankh extends Item {
     public ArrayList<String> actions( Hero hero ) {
         ArrayList<String> actions = super.actions(hero);
         DewVial vial = hero.belongings.getItem(DewVial.class);
-        if (vial != null && vial.isFull())
+        if (vial != null && vial.isFull() && !blessed)
             actions.add( AC_BLESS );
         return actions;
     }
