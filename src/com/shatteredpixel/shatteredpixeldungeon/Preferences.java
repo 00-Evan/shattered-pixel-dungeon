7448bfea88389e99b7f5ae98c9902131b49531b6
Evan Debenham <Evan.SHPX@gmail.com>
2014-12-16 22:48:14 -0500

    v0.2.3: removed additional unused reference.

--- src/com/shatteredpixel/shatteredpixeldungeon/Preferences.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/Preferences.java
@@ -33,7 +33,6 @@ enum Preferences {
 	public static final String KEY_ZOOM			= "zoom";
 	public static final String KEY_LAST_CLASS	= "last_class";
     public static final String KEY_CHALLENGES	= "challenges";
-	public static final String KEY_DONATED		= "donated";
 	public static final String KEY_INTRO		= "intro";
 	public static final String KEY_BRIGHTNESS	= "brightness";
     public static final String KEY_VERSION      = "version";
