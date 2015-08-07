d73e663ef6d8d63f750eae587147c2a18d543e92
Evan Debenham <Evan@ShatteredPixel.com>
2015-08-07 16:31:20 -0400

    v0.3.1: invisibility now lasts for 20 rounds instead of 15.

--- src/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Invisibility.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Invisibility.java
@@ -29,7 +29,7 @@ import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
 
 public class Invisibility extends FlavourBuff {
 
-	public static final float DURATION	= 15f;
+	public static final float DURATION	= 20f;
 
 	{
 		type = buffType.POSITIVE;
