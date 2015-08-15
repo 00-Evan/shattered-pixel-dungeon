1581b18f11e3857c539259eb98e89c25e06d55b1
Evan Debenham <Evan.SHPX@gmail.com>
2015-08-14 22:18:58 -0400

    v0.3.1: added Guardian Trap

---
+++ src/com/shatteredpixel/shatteredpixeldungeon/levels/traps/GuardianTrap.java
@@ -0,0 +1,122 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015  Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2015 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.shatteredpixel.shatteredpixeldungeon.levels.traps;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.Statistics;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Statue;
+import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.StatueSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.TrapSprite;
+import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.watabou.noosa.audio.Sample;
+
+public class GuardianTrap extends Trap {
+
+	{
+		name = "Guardian trap";
+		color = TrapSprite.RED;
+		shape = TrapSprite.STARS;
+	}
+
+	@Override
+	public void activate() {
+
+		for (Mob mob : Dungeon.level.mobs) {
+			mob.beckon( pos );
+		}
+
+		if (Dungeon.visible[pos]) {
+			GLog.w("The trap emits a piercing sound that echoes throughout the dungeon!");
+			CellEmitter.center(pos).start( Speck.factory(Speck.SCREAM), 0.3f, 3 );
+		}
+
+		Sample.INSTANCE.play( Assets.SND_ALERT );
+
+		for (int i = 0; i < (Dungeon.depth - 5)/5; i++){
+			Guardian guardian = new Guardian();
+			guardian.state = guardian.WANDERING;
+			guardian.pos = Dungeon.level.randomRespawnCell();
+			GameScene.add(guardian);
+			guardian.beckon(Dungeon.hero.pos );
+		}
+
+	}
+
+	@Override
+	public String desc() {
+		return "This trap is tied to a strange magical mechanism, " +
+				"which will summon guardians and alert all enemies on the floor.";
+	}
+
+	public static class Guardian extends Statue {
+
+		{
+			name = "summoned guardian";
+			spriteClass = GuardianSprite.class;
+
+			EXP = 0;
+			state = WANDERING;
+		}
+
+		public Guardian(){
+			super();
+
+			weapon.enchant(null);
+			weapon.degrade(weapon.level);
+		}
+
+		@Override
+		public void beckon(int cell) {
+			//Beckon works on these ones, unlike their superclass.
+			notice();
+
+			if (state != HUNTING) {
+				state = WANDERING;
+			}
+			target = cell;
+		}
+
+		@Override
+		public String description() {
+			return "This blue apparition seems to be a summoned echo of one of the dungeon's stone guardians." +
+					"\n\nWhile the statue itself is almost incorporeal, the _" + weapon.name() + "_, it's wielding, looks real.";
+		}
+	}
+
+	public static class GuardianSprite extends StatueSprite {
+
+		public GuardianSprite(){
+			super();
+			tint(0, 0, 1, 0.2f);
+		}
+
+		@Override
+		public void resetColor() {
+			super.resetColor();
+			tint(0, 0, 1, 0.2f);
+		}
+	}
+}
