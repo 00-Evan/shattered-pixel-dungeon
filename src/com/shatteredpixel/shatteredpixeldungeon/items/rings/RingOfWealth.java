67b439dc1d406de36cc8f291294c3e9df84f6482
Evan Debenham <Evan@ShatteredPixel.com>
2015-08-21 09:19:08 -0400

    v0.3.1a: fixed a typo in ring of wealth description

--- src/com/shatteredpixel/shatteredpixeldungeon/items/rings/RingOfWealth.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/items/rings/RingOfWealth.java
@@ -35,7 +35,7 @@ public class RingOfWealth extends Ring {
 	public String desc() {
 		return isKnown() ?
 				"It's not clear what this ring does exactly, good luck may influence " +
-				"the life an an adventurer in many subtle ways. " +
+				"the life of an adventurer in many subtle ways. " +
 				"Naturally a degraded ring would give bad luck." :
 				super.desc();
 	}
