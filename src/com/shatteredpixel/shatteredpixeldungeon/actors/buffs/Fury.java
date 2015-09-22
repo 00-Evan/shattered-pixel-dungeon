855db312da0f023c123d979a76abf7d046a08982
Evan Debenham <Evan.SHPX@gmail.com>
2015-09-22 03:31:01 -0400

    v0.3.2: buffed fury health % trigger

--- src/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Fury.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Fury.java
@@ -24,7 +24,7 @@ import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
 
 public class Fury extends Buff {
 	
-	public static float LEVEL	= 0.4f;
+	public static float LEVEL	= 0.5f;
 
 	{
 		type = buffType.POSITIVE;
