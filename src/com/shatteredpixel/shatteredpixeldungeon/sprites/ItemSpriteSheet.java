c5a8ed0178ab7fcccb3b7e30390ea38c42c33c42
Evan Debenham <Evan.SHPX@gmail.com>
2015-09-29 18:49:42 -0400

    v0.3.2: added some images for new sewers quests

--- src/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
@@ -224,7 +224,8 @@ public class ItemSpriteSheet {
 	//Row Fourteen: Quest Items
 	public static final int SKULL	= ROW14+0;
 	public static final int DUST	= ROW14+1;
-	public static final int PICKAXE	= ROW14+2;
+	public static final int CANDLE	= ROW14+2;
+	public static final int PICKAXE	= ROW14+3;
 	public static final int ORE		= ROW14+3;
 	public static final int TOKEN	= ROW14+4;
 
