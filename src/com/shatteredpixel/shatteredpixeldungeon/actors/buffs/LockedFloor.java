7999de94ac4ca2495ef7e2af794a9a2433890812
Evan Debenham <Evan.SHPX@gmail.com>
2015-11-07 21:38:05 -0500

    v0.3.2b: locked floor count now persists over save/load, also increased base amount to 50

--- src/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/LockedFloor.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/LockedFloor.java
@@ -22,11 +22,12 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.buffs;
 
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
+import com.watabou.utils.Bundle;
 
 public class LockedFloor extends Buff {
 
 	//the amount of turns remaining before beneficial passive effects turn off
-	private float left = 20; //starts at 20 turns
+	private float left = 50; //starts at 50 turns
 
 	@Override
 	public boolean act() {
@@ -47,6 +48,20 @@ public class LockedFloor extends Buff {
 
 	public boolean regenOn(){
 		return left >= 1;
+	}
+
+	private final String LEFT = "left";
+
+	@Override
+	public void storeInBundle(Bundle bundle) {
+		super.storeInBundle(bundle);
+		bundle.put( LEFT, left );
+	}
+
+	@Override
+	public void restoreFromBundle(Bundle bundle) {
+		super.restoreFromBundle(bundle);
+		left = bundle.getFloat( LEFT );
 	}
 
 	@Override
