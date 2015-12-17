66ee082503460f18ace95936285fec82864c5b69
Evan Debenham <Evan.SHPX@gmail.com>
2015-12-16 22:45:19 -0500

    v0.3.3: updated copyright on maze painter

--- src/com/shatteredpixel/shatteredpixeldungeon/levels/painters/MazePainter.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/levels/painters/MazePainter.java
@@ -1,3 +1,23 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015  Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2015 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package com.shatteredpixel.shatteredpixeldungeon.levels.painters;
 
 import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
@@ -5,9 +25,6 @@ import com.shatteredpixel.shatteredpixeldungeon.levels.Room;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.watabou.utils.Random;
 
-/**
- * Created by Evan on 24/11/2015.
- */
 public class MazePainter extends Painter {
 
 	public static void paint( Level level, Room room ) {
