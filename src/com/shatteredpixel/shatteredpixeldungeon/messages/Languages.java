3333a1c4fe9ccb329da25366416c52b7abb6c500
Evan Debenham <Evan.SHPX@gmail.com>
2016-02-21 21:03:22 -0500

    v0.3.4b: updated translation credits

--- src/com/shatteredpixel/shatteredpixeldungeon/messages/Languages.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/messages/Languages.java
@@ -26,15 +26,15 @@ public enum Languages {
 	ENGLISH("english",      "", Status.REVIEWED, null, null),
 	RUSSIAN("русский",      "ru", Status.REVIEWED, new String[]{"ConsideredHamster", "Inevielle", "yarikonline"}, new String[]{"HerrGotlieb", "Shamahan"}),
 	KOREAN("한국어",         "ko", Status.REVIEWED, new String[]{"Flameblast12"}, new String[]{"Ddojin0115", "Eeeei", "lsiebnie", "WondarRabb1t"}),
-	CHINESE("中文",          "zh", Status.REVIEWED, new String[]{"Jinkeloid(zdx00793)"}, new String[]{"931451545", "HoofBumpBlurryface", "Lyn-0401", "ShatteredFlameBlast", "Tempest102"}),
+	CHINESE("中文",          "zh", Status.REVIEWED, new String[]{"Jinkeloid(zdx00793)"}, new String[]{"931451545", "HoofBumpBlurryface", "Lery", "Lyn-0401", "ShatteredFlameBlast", "Tempest102"}),
 
 	PORTUGUESE("português", "pt", Status.UNREVIEWED, new String[]{"Matheus208"}, new String[]{"JST", "Try31"}),
 	GERMAN("deutsch",       "de", Status.UNREVIEWED, new String[]{"Davedude", "KrystalCroft"}, new String[]{"Dallukas", "DarkPixel", "ErichME", "Sarius", "ThunfischGott", "Zap0", "Oragothen"}),
 	FRENCH("français",      "fr", Status.UNREVIEWED, null, new String[]{"Alsydis", "Basttee", "Canc42", "Go11um", "Kultissim", "Minikrob", "Solthaar"}),
 
 	POLISH("polski",        "pl", Status.INCOMPLETE, null, new String[]{"Darden", "Deksippos", "Scharnvirk", "Wawrzyn", "Dusakus", "Michaub"}),
-	SPANISH("español",      "es", Status.INCOMPLETE, null, new String[]{"CorvosUtopy", "LucasCamilo", "Luuciano96", "Pecheneg", "Prancer", "Talruin", "Alfongad", "Ctrijueque", "Grayscales", "Jonismack1", "Pixeled4life"}),
-	ITALIAN("italiano",		"it", Status.INCOMPLETE, null, new String[]{"4est", "Bizzolino", "Funnydwarf", "Nessunluogo", "Umby000"});
+	SPANISH("español",      "es", Status.INCOMPLETE, null, new String[]{"CorvosUtopy", "Kiroto", "LucasCamilo", "Luuciano96", "Pecheneg", "Prancer", "Talruin", "Alfongad", "Ctrijueque", "Grayscales", "Jonismack1", "Pixeled4life"}),
+	ITALIAN("italiano",		"it", Status.INCOMPLETE, null, new String[]{"4est", "DaniMare", "Danzl", "Bizzolino", "Funnydwarf", "Nessunluogo", "Umby000"});
 
 	public enum Status{
 		//below 60% complete languages are not added.
