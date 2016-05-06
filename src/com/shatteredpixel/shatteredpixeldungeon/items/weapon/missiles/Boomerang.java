90db5b8c1fd32d6377739d69982052415aa84c20
Evan Debenham <Evan.SHPX@gmail.com>
2016-05-06 18:19:12 -0400

    v0.3.5a: fixed an equipped boomerang removing items from quickslots when thrown

--- src/com/shatteredpixel/shatteredpixeldungeon/items/weapon/missiles/Boomerang.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/items/weapon/missiles/Boomerang.java
@@ -119,6 +119,7 @@ public class Boomerang extends MissileWeapon {
 	@Override
 	public void cast( Hero user, int dst ) {
 		throwEquiped = isEquipped( user );
+		if (throwEquiped) Dungeon.quickslot.convertToPlaceholder(this);
 		super.cast( user, dst );
 	}
 	
