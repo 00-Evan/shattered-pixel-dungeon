2bb55aa7c15d62e66b6bcd8a9518c161494ada3c
Evan Debenham <Evan@ShatteredPixel.com>
2016-10-22 22:42:52 -0400

    v0.4.3a: fixed a 'no string found' error in corpsedust

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/CorpseDust.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/CorpseDust.java
@@ -67,7 +67,7 @@ public class CorpseDust extends Item {
 	@Override
 	public boolean doPickUp(Hero hero) {
 		if (super.doPickUp(hero)){
-			GLog.n( Messages.get("chill") );
+			GLog.n( Messages.get( this, "chill") );
 			Buff.affect(hero, DustGhostSpawner.class);
 			return true;
 		}
