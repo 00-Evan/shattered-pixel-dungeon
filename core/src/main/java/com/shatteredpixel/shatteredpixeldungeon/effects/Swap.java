ba8598132aaf5007950ca818f36d850306be7023
Evan Debenham <Evan.SHPX@gmail.com>
2017-02-22 15:10:20 -0500

    updated copyright

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/Swap.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/Swap.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2012-2015  Oleg Dolya
  *
  * Shattered Pixel Dungeon
- * Copyright (C) 2014-2016 Evan Debenham
+ * Copyright (C) 2014-2017 Evan Debenham
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+
 package com.shatteredpixel.shatteredpixeldungeon.effects;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
