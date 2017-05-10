7a197a1fe8828362a0f9032354ff310d684f3cdf
Evan Debenham <Evan.SHPX@gmail.com>
2017-05-09 22:51:08 -0400

    v0.6.0: improved burned room logic

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/BurnedRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/BurnedRoom.java
@@ -25,34 +25,51 @@ import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.traps.FireTrap;
+import com.watabou.utils.Point;
 import com.watabou.utils.Random;
 
-public class BurnedRoom extends StandardRoom {
+public class BurnedRoom extends PatchRoom {
+	
+	@Override
+	public float[] sizeCatProbs() {
+		return new float[]{4, 1, 0};
+	}
 	
 	@Override
 	public void paint(Level level) {
 		Painter.fill( level, this, Terrain.WALL );
+		Painter.fill( level, this, 1, Terrain.EMPTY );
 		for (Door door : connected.values()) {
 			door.set( Door.Type.REGULAR );
 		}
 		
+		//past 8x8 each point of width/height decreases fill by 3%
+		// e.g. a 14x14 burned room has a fill of 54%
+		float fill = Math.min( 1f, 1.48f - (width()+height())*0.03f);
+		setupPatch(level, fill, 2, false );
+		
 		for (int i=top + 1; i < bottom; i++) {
 			for (int j=left + 1; j < right; j++) {
+				if (!patch[xyToPatchCoords(j, i)])
+					continue;
 				int cell = i * level.width() + j;
-				int t = Terrain.EMBERS;
+				int t;
 				switch (Random.Int( 5 )) {
-					case 0:
+					case 0: default:
 						t = Terrain.EMPTY;
 						break;
 					case 1:
+						t = Terrain.EMBERS;
+						break;
+					case 2:
 						t = Terrain.TRAP;
 						level.setTrap(new FireTrap().reveal(), cell);
 						break;
-					case 2:
+					case 3:
 						t = Terrain.SECRET_TRAP;
 						level.setTrap(new FireTrap().hide(), cell);
 						break;
-					case 3:
+					case 4:
 						t = Terrain.INACTIVE_TRAP;
 						FireTrap trap = new FireTrap();
 						trap.reveal().active = false;
@@ -63,4 +80,15 @@ public class BurnedRoom extends StandardRoom {
 			}
 		}
 	}
+	
+	@Override
+	public boolean canModifyTerrain(Point p) {
+		return super.canModifyTerrain(p) && !patch[xyToPatchCoords(p.x, p.y)];
+	}
+	
+	@Override
+	public boolean canPlaceTrap(Point p) {
+		return super.canPlaceTrap(p) && !patch[xyToPatchCoords(p.x, p.y)];
+	}
+	
 }
