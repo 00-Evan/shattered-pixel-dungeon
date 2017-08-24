4c0f588489bed4dbee22b13e1b18a53b90269f49
Evan Debenham <Evan.SHPX@gmail.com>
2017-08-24 08:46:13 -0400

    v0.6.1b: fixed concurrent modification exceptions when gettin buffs

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
@@ -314,8 +314,8 @@ public abstract class Char extends Actor {
 		super.spend( time / timeScale );
 	}
 	
-	public HashSet<Buff> buffs() {
-		return buffs;
+	public synchronized HashSet<Buff> buffs() {
+		return new HashSet<>(buffs);
 	}
 	
 	@SuppressWarnings("unchecked")
