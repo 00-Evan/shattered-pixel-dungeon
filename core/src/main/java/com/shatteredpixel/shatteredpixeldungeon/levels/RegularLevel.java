9ad765e2564eaddee064aa258e83d8e02c9e1af3
Evan Debenham <Evan.SHPX@gmail.com>
2017-10-31 03:43:00 -0400

    v0.6.2a: reduced the chance for golden chests to appear

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/RegularLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/RegularLevel.java
@@ -325,7 +325,7 @@ public abstract class RegularLevel extends Level {
 			
 			Item toDrop = Generator.random();
 			if ((toDrop instanceof Artifact && Random.Int(2) == 0) ||
-					(toDrop.isUpgradable() && Random.Int(3) <= toDrop.level())){
+					(toDrop.isUpgradable() && Random.Int(4 - toDrop.level()) == 0)){
 				Heap dropped = drop( toDrop, cell );
 				if (heaps.get(cell) == dropped) {
 					dropped.type = Heap.Type.LOCKED_CHEST;
