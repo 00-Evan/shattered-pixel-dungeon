27e0f5843b7d2be6f901fc04706e686d561d6ec8
Evan Debenham <Evan@ShatteredPixel.com>
2018-02-19 14:43:28 -0500

    v0.6.3a: fixed rare crashes caused by ambitious imp quest reward

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndImp.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndImp.java
@@ -71,6 +71,7 @@ public class WndImp extends Window {
 		hide();
 		
 		tokens.detachAll( Dungeon.hero.belongings.backpack );
+		if (reward == null) return;
 
 		reward.identify();
 		if (reward.doPickUp( Dungeon.hero )) {
