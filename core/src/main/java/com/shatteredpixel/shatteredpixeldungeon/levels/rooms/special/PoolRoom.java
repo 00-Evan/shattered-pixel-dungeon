d739ae30a295c407a3174414470a24a4ce6dceff
Evan Debenham <Evan.SHPX@gmail.com>
2018-03-30 17:27:22 -0400

    v0.6.4: Pool rooms now have a one tile buffer at the door

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/special/PoolRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/special/PoolRoom.java
@@ -37,7 +37,17 @@ public class PoolRoom extends SpecialRoom {
 
 	private static final int NPIRANHAS	= 3;
 	
-	public void paint( Level level ) {
+	@Override
+	public int minWidth() {
+		return 6;
+	}
+	
+	@Override
+	public int minHeight() {
+		return 6;
+	}
+	
+	public void paint(Level level ) {
 		
 		Painter.fill( level, this, Terrain.WALL );
 		Painter.fill( level, this, 1, Terrain.WATER );
@@ -51,21 +61,25 @@ public class PoolRoom extends SpecialRoom {
 			
 			x = right - 1;
 			y = top + height() / 2;
+			Painter.fill(level, left+1, top+1, 1, height()-2, Terrain.EMPTY_SP);
 			
 		} else if (door.x == right) {
 			
 			x = left + 1;
 			y = top + height() / 2;
+			Painter.fill(level, right-1, top+1, 1, height()-2, Terrain.EMPTY_SP);
 			
 		} else if (door.y == top) {
 			
 			x = left + width() / 2;
 			y = bottom - 1;
+			Painter.fill(level, left+1, top+1, width()-2, 1, Terrain.EMPTY_SP);
 			
 		} else if (door.y == bottom) {
 			
 			x = left + width() / 2;
 			y = top + 1;
+			Painter.fill(level, left+1, bottom-1, width()-2, 1, Terrain.EMPTY_SP);
 			
 		}
 		
