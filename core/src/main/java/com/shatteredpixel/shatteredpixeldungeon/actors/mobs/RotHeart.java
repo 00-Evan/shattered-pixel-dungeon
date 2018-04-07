a8351e48fc0cfccd5696a5c9645dc73326ee0745
Evan Debenham <Evan.SHPX@gmail.com>
2018-04-06 23:56:30 -0400

    v0.6.4a: rot heart is now immune to additional debuffs

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/RotHeart.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/RotHeart.java
@@ -25,8 +25,12 @@ import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Blob;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.ToxicGas;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Amok;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Burning;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Paralysis;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Sleep;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Terror;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Vertigo;
 import com.shatteredpixel.shatteredpixeldungeon.plants.Rotberry;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.RotHeartSprite;
@@ -113,8 +117,12 @@ public class RotHeart extends Mob {
 	}
 	
 	{
+		immunities.add( Paralysis.class );
+		immunities.add( Amok.class );
+		immunities.add( Sleep.class );
 		immunities.add( ToxicGas.class );
 		immunities.add( Terror.class );
+		immunities.add( Vertigo.class );
 	}
 
 }
