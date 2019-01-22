1613c8adc4476fe74cbc4b4b003993181dbe22bc
Evan Debenham <Evan@ShatteredPixel.com>
2019-01-22 15:45:45 -0500

    v0.7.2: corrected incorrect item spritesheet values for brews

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
@@ -546,11 +546,11 @@ public class ItemSpriteSheet {
 	private static final int BREWS          =                               xy(1, 25);  //8 slots
 	public static final int BREW_INFERNAL   = BREWS+0;
 	public static final int BREW_BLIZZARD   = BREWS+1;
-	public static final int BREW_SHOCKING   = BREWS+3;
-	public static final int BREW_CAUSTIC    = BREWS+4;
-	public static final int BREW_WICKED     = BREWS+5; //discontinued
-	public static final int BREW_FRIGID     = BREWS+6; //discontinued
-	public static final int BREW_FROSTFIRE  = BREWS+7; //discontinued
+	public static final int BREW_SHOCKING   = BREWS+2;
+	public static final int BREW_CAUSTIC    = BREWS+3;
+	public static final int BREW_WICKED     = BREWS+4; //discontinued
+	public static final int BREW_FRIGID     = BREWS+5; //discontinued
+	public static final int BREW_FROSTFIRE  = BREWS+6; //discontinued
 	
 	private static final int ELIXIRS        =                               xy(9, 25);  //8 slots
 	public static final int ELIXIR_HONEY    = ELIXIRS+0;
