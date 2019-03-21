6c44a5c801aaf70f5ecd9ac8f447ca3ef6e59bd9
Evan Debenham <Evan@ShatteredPixel.com>
2019-03-21 01:54:49 -0400

    v0.7.2a: reverted unintended debug proc rate for elastic

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/enchantments/Elastic.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/enchantments/Elastic.java
@@ -39,7 +39,7 @@ public class Elastic extends Weapon.Enchantment {
 		// lvl 2 - 43%
 		int level = Math.max( 0, weapon.level() );
 		
-		if (Random.Int( level + 5000 ) >= 4) {
+		if (Random.Int( level + 5 ) >= 4) {
 			//trace a ballistica to our target (which will also extend past them
 			Ballistica trajectory = new Ballistica(attacker.pos, defender.pos, Ballistica.STOP_TARGET);
 			//trim it to just be the part that goes past them
