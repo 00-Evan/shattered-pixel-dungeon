329ad0e14f364cff04319bfa8a0033a89a00e990
Evan Debenham <Evan@ShatteredPixel.com>
2019-06-19 22:22:03 -0400

    v0.7.4: wand names are now in title case in the wandmaker reward window

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndWandmaker.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndWandmaker.java
@@ -66,7 +66,7 @@ public class WndWandmaker extends Window {
 		message.setPos(0, titlebar.bottom() + GAP);
 		add( message );
 		
-		RedButton btnWand1 = new RedButton( Wandmaker.Quest.wand1.name() ) {
+		RedButton btnWand1 = new RedButton( Messages.titleCase(Wandmaker.Quest.wand1.name()) ) {
 			@Override
 			protected void onClick() {
 				selectReward( wandmaker, item, Wandmaker.Quest.wand1 );
@@ -75,7 +75,7 @@ public class WndWandmaker extends Window {
 		btnWand1.setRect(0, message.top() + message.height() + GAP, WIDTH, BTN_HEIGHT);
 		add( btnWand1 );
 		
-		RedButton btnWand2 = new RedButton( Wandmaker.Quest.wand2.name() ) {
+		RedButton btnWand2 = new RedButton( Messages.titleCase(Wandmaker.Quest.wand2.name()) ) {
 			@Override
 			protected void onClick() {
 				selectReward( wandmaker, item, Wandmaker.Quest.wand2 );
