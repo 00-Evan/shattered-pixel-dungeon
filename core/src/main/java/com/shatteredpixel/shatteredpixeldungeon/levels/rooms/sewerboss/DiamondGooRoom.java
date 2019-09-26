21a719e0c51c3db5f00b1a24b6ddcc752fb5718c
Evan Debenham <Evan@ShatteredPixel.com>
2019-09-26 18:43:24 -0400

    v0.7.5: fixed additional crash errors caused by diamond Goo rooms

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/sewerboss/DiamondGooRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/sewerboss/DiamondGooRoom.java
@@ -35,6 +35,8 @@ public class DiamondGooRoom extends GooBossRoom {
 		
 		//we want the end width to be width()-2, and the width will grow by a total of (height()-4 - height()%2)
 		int diamondWidth = width()-2 - (height()-4 - height()%2);
+		//but starting width cannot be smaller than 2 on even width, 3 on odd width.
+		diamondWidth = Math.max(diamondWidth, width()%2 == 0 ? 2 : 3);
 		
 		for (int i = 1; i < height(); i++){
 			Painter.fill( level, left + (width() - diamondWidth)/2, top+i, diamondWidth, height()-2*i, Terrain.EMPTY);
