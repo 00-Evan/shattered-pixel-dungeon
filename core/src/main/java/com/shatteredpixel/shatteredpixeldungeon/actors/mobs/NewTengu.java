868ace78b1c16ad06aff9afc9d039f08a6aaa042
Evan Debenham <Evan@ShatteredPixel.com>
2019-09-29 00:04:22 -0400

    v0.7.5: fixed Tengu not being able to see the player at high distances

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NewTengu.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NewTengu.java
@@ -87,6 +87,8 @@ public class NewTengu extends Mob {
 		flying = true; //doesn't literally fly, but he is fleet-of-foot enough to avoid hazards
 		
 		properties.add(Property.BOSS);
+		
+		viewDistance = 12;
 	}
 	
 	@Override
