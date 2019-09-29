57a570db40fec1a667abbdd6fffefec0ba8ecc02
Evan Debenham <Evan@ShatteredPixel.com>
2019-09-28 23:34:27 -0400

    v0.7.5: sped up slime attack animation, now consistent with other mobs

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/SlimeSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/SlimeSprite.java
@@ -39,8 +39,8 @@ public class SlimeSprite extends MobSprite {
 		run = new Animation( 10, true );
 		run.frames( frames, 0, 2, 3, 3, 2, 0 );
 		
-		attack = new Animation( 10, false );
-		attack.frames( frames, 2, 3, 4, 5, 2 );
+		attack = new Animation( 15, false );
+		attack.frames( frames, 2, 3, 4, 6, 5 );
 		
 		die = new Animation( 10, false );
 		die.frames( frames, 0, 5, 6, 7 );
