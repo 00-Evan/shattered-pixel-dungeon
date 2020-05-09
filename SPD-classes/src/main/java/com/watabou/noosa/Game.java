ff08ee3b7f08fc1880ff33ff28d016774bf25490
Evan Debenham <Evan@ShatteredPixel.com>
2020-05-09 19:38:09 -0400

    v0.8.1: gl_scissor node is now always disabled at the end of a frame This fixes a bug in some screen recorders when windows are open

--- SPD-classes/src/main/java/com/watabou/noosa/Game.java
+++ SPD-classes/src/main/java/com/watabou/noosa/Game.java
@@ -138,7 +138,8 @@ public class Game implements ApplicationListener {
 		Gdx.gl.glDisable(Gdx.gl.GL_SCISSOR_TEST);
 		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
 		draw();
-		
+
+		Gdx.gl.glDisable( Gdx.gl.GL_SCISSOR_TEST );
 		Gdx.gl.glFlush();
 		
 		step();
