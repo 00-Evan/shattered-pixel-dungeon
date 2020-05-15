72a97fd83bb828a7c36561197892043297941b87
Evan Debenham <Evan@ShatteredPixel.com>
2020-05-15 12:21:45 -0400

    v0.8.1: improved ring icons

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
@@ -716,13 +716,13 @@ public class ItemSpriteSheet {
 			assignIconRect( RING_ELEMENTS,      7, 7 );
 			assignIconRect( RING_ENERGY,        7, 5 );
 			assignIconRect( RING_EVASION,       7, 7 );
-			assignIconRect( RING_FORCE,         7, 5 );
-			assignIconRect( RING_FUROR,         6, 6 );
+			assignIconRect( RING_FORCE,         5, 6 );
+			assignIconRect( RING_FUROR,         7, 6 );
 			assignIconRect( RING_HASTE,         6, 6 );
 			assignIconRect( RING_MIGHT,         7, 7 );
 			assignIconRect( RING_SHARPSHOOT,    7, 7 );
 			assignIconRect( RING_TENACITY,      6, 6 );
-			assignIconRect( RING_WEALTH,        6, 6 );
+			assignIconRect( RING_WEALTH,        7, 6 );
 		}
 
 		                                                                                //16 free slots
