6a3cd94d224b7456e2df5c433e6b0fa031b716d4
Evan Debenham <Evan@ShatteredPixel.com>
2020-08-25 15:04:14 -0400

    v0.8.2c: fixed acidic scorpios not dropping experience potions

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Acidic.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Acidic.java
@@ -25,6 +25,7 @@ import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Ooze;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfExperience;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.AcidicSprite;
 
@@ -51,5 +52,9 @@ public class Acidic extends Scorpio {
 		}
 		return super.defenseProc( enemy, damage );
 	}
-	
+
+	@Override
+	protected Item createLoot() {
+		return new PotionOfExperience();
+	}
 }
