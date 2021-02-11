0131feb0b9ed9c4fb149fd434f28b01b7e9a972e
Evan Debenham <Evan@ShatteredPixel.com>
2021-02-11 13:47:54 -0500

    v0.9.2: fixed succubus vfx/sfx triggering even if they aren't visible

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Succubus.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Succubus.java
@@ -83,14 +83,18 @@ public class Succubus extends Mob {
 			} else {
 				HP += 5 + damage;
 			}
-			sprite.emitter().burst( Speck.factory( Speck.HEALING ), 2 );
-			Sample.INSTANCE.play( Assets.Sounds.CHARMS );
+			if (Dungeon.level.heroFOV[pos]) {
+				sprite.emitter().burst( Speck.factory( Speck.HEALING ), 2 );
+				Sample.INSTANCE.play( Assets.Sounds.CHARMS );
+			}
 		} else if (Random.Int( 3 ) == 0) {
 			Charm c = Buff.affect( enemy, Charm.class, Charm.DURATION/2f );
 			c.object = id();
 			c.ignoreNextHit = true; //so that the -5 duration from succubus hit is ignored
-			enemy.sprite.centerEmitter().start( Speck.factory( Speck.HEART ), 0.2f, 5 );
-			Sample.INSTANCE.play( Assets.Sounds.CHARMS );
+			if (Dungeon.level.heroFOV[enemy.pos]) {
+				enemy.sprite.centerEmitter().start(Speck.factory(Speck.HEART), 0.2f, 5);
+				Sample.INSTANCE.play(Assets.Sounds.CHARMS);
+			}
 		}
 		
 		return damage;
