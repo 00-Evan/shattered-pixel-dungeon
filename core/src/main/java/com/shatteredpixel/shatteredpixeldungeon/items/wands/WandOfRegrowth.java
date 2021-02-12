b7f2b29554454fe419157c2ac6314de7d4912434
Evan Debenham <Evan@ShatteredPixel.com>
2021-02-11 22:48:55 -0500

    v0.9.2: tweaked wand of regrowth charge limit and added it to desc

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfRegrowth.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfRegrowth.java
@@ -62,6 +62,7 @@ public class WandOfRegrowth extends Wand {
 	}
 	
 	private int totChrgUsed = 0;
+	private int chargesOverLimit = 0;
 
 	ConeAOE cone;
 	int target;
@@ -81,8 +82,10 @@ public class WandOfRegrowth extends Wand {
 
 		ArrayList<Integer> cells = new ArrayList<>(cone.cells);
 
-		int overLimit = totChrgUsed - chargeLimit(Dungeon.hero.lvl);
-		float furrowedChance = overLimit > 0 ? (overLimit / (10f + Dungeon.hero.lvl)) : 0;
+		float furrowedChance = 0;
+		if (totChrgUsed >= chargeLimit(Dungeon.hero.lvl)){
+			furrowedChance = (chargesOverLimit+1)/5f;
+		}
 
 		int chrgUsed = chargesPerCast();
 		int grassToPlace = Math.round((3.67f+buffedLvl()/3f)*chrgUsed);
@@ -175,9 +178,17 @@ public class WandOfRegrowth extends Wand {
 			grassToPlace--;
 		}
 
-		if (furrowedChance < 1f) {
+		if (totChrgUsed < chargeLimit(Dungeon.hero.lvl)) {
+			chargesOverLimit = 0;
 			totChrgUsed += chrgUsed;
+			if (totChrgUsed > chargeLimit(Dungeon.hero.lvl)){
+				chargesOverLimit = totChrgUsed - chargeLimit(Dungeon.hero.lvl);
+				totChrgUsed = chargeLimit(Dungeon.hero.lvl);
+			}
+		} else {
+			chargesOverLimit += chrgUsed;
 		}
+
 	}
 	
 	private int chargeLimit( int heroLvl ){
@@ -254,7 +265,12 @@ public class WandOfRegrowth extends Wand {
 
 	@Override
 	public String statsDesc() {
-		return Messages.get(this, "stats_desc", chargesPerCast());
+		String desc = Messages.get(this, "stats_desc", chargesPerCast());
+		if (isIdentified()){
+			int chargeLeft = chargeLimit(Dungeon.hero.lvl) - totChrgUsed;
+			if (chargeLeft < 10000) desc += " " + Messages.get(this, "degradation", Math.max(chargeLeft, 0));
+		}
+		return desc;
 	}
 
 	@Override
@@ -270,17 +286,20 @@ public class WandOfRegrowth extends Wand {
 	}
 	
 	private static final String TOTAL = "totChrgUsed";
+	private static final String OVER = "chargesOverLimit";
 	
 	@Override
 	public void storeInBundle(Bundle bundle) {
 		super.storeInBundle(bundle);
 		bundle.put( TOTAL, totChrgUsed );
+		bundle.put( OVER, chargesOverLimit);
 	}
 	
 	@Override
 	public void restoreFromBundle(Bundle bundle) {
 		super.restoreFromBundle(bundle);
 		totChrgUsed = bundle.getInt(TOTAL);
+		chargesOverLimit = bundle.getInt(OVER);
 	}
 	
 	public static class Dewcatcher extends Plant{
