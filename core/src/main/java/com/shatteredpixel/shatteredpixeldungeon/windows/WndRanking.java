ff8264a528e86753a177e4fb2e724380427d2470
Evan Debenham <Evan@ShatteredPixel.com>
2021-05-22 21:08:50 -0400

    v0.9.3: fixed T4 talents not appearing in rankings

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndRanking.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndRanking.java
@@ -181,6 +181,7 @@ public class WndRanking extends WndTabbed {
 					int tiers = 1;
 					if (Dungeon.hero.lvl >= 6) tiers++;
 					if (Dungeon.hero.lvl >= 12 && Dungeon.hero.subClass != HeroSubClass.NONE) tiers++;
+					if (Dungeon.hero.lvl >= 20 && Dungeon.hero.armorAbility != null) tiers++;
 					while (Dungeon.hero.talents.size() > tiers){
 						Dungeon.hero.talents.remove(Dungeon.hero.talents.size()-1);
 					}
