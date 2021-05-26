a13ca21337c4e009b8acb2f9310a067155c737fa
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 18:46:46 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/bags/VelvetPouch.java
@@ -0,0 +1,57 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.items.bags;
+
+
+import com.elementalpixel.elementalpixeldungeon.items.Item;
+import com.elementalpixel.elementalpixeldungeon.items.quest.GooBlob;
+import com.elementalpixel.elementalpixeldungeon.items.quest.MetalShard;
+import com.elementalpixel.elementalpixeldungeon.items.stones.Runestone;
+import com.elementalpixel.elementalpixeldungeon.plants.Plant;
+import com.elementalpixel.elementalpixeldungeon.sprites.ItemSpriteSheet;
+
+public class VelvetPouch extends Bag {
+
+	{
+		image = ItemSpriteSheet.POUCH;
+	}
+
+	@Override
+	public boolean canHold( Item item ) {
+		if (item instanceof Plant.Seed || item instanceof Runestone
+				|| item instanceof GooBlob || item instanceof MetalShard){
+			return super.canHold(item);
+		} else {
+			return false;
+		}
+	}
+
+	public int capacity(){
+		return 19;
+	}
+	
+	@Override
+	public int value() {
+		return 30;
+	}
+
+}
