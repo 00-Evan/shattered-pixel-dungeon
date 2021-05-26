3984dbfcac2c18fe97c6afb5452bf7ced688e6f9
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 18:33:17 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/Challenges.java
@@ -0,0 +1,75 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon;
+
+
+import com.elementalpixel.elementalpixeldungeon.items.Dewdrop;
+import com.elementalpixel.elementalpixeldungeon.items.Item;
+
+public class Challenges {
+
+	//Some of these internal IDs are outdated and don't represent what these challenges do
+	public static final int NO_FOOD				= 1;
+	public static final int NO_ARMOR			= 2;
+	public static final int NO_HEALING			= 4;
+	public static final int NO_HERBALISM		= 8;
+	public static final int SWARM_INTELLIGENCE	= 16;
+	public static final int DARKNESS			= 32;
+	public static final int NO_SCROLLS		    = 64;
+	public static final int CHAMPION_ENEMIES	= 128;
+
+	public static final int MAX_VALUE           = 255;
+
+	public static final String[] NAME_IDS = {
+			"champion_enemies",
+			"no_food",
+			"no_armor",
+			"no_healing",
+			"no_herbalism",
+			"swarm_intelligence",
+			"darkness",
+			"no_scrolls"
+	};
+
+	public static final int[] MASKS = {
+			CHAMPION_ENEMIES, NO_FOOD, NO_ARMOR, NO_HEALING, NO_HERBALISM, SWARM_INTELLIGENCE, DARKNESS, NO_SCROLLS
+	};
+
+	public static int activeChallenges(){
+		int chCount = 0;
+		for (int ch : Challenges.MASKS){
+			if ((Dungeon.challenges & ch) != 0) chCount++;
+		}
+		return chCount;
+	}
+
+	public static boolean isItemBlocked( Item item ){
+
+		if (Dungeon.isChallenged(NO_HERBALISM) && item instanceof Dewdrop){
+			return true;
+		}
+
+		return false;
+
+	}
+
+}
