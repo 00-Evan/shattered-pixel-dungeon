65043b2a9b58c53c501780d7ec9b4312a1dd8c2d
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 19:20:17 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/levels/HallsLevel.java
@@ -0,0 +1,262 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.levels;
+
+
+import com.elementalpixel.elementalpixeldungeon.Assets;
+import com.elementalpixel.elementalpixeldungeon.Dungeon;
+import com.elementalpixel.elementalpixeldungeon.items.Torch;
+import com.elementalpixel.elementalpixeldungeon.levels.painters.HallsPainter;
+import com.elementalpixel.elementalpixeldungeon.levels.painters.Painter;
+import com.elementalpixel.elementalpixeldungeon.levels.rooms.Room;
+import com.elementalpixel.elementalpixeldungeon.levels.rooms.special.DemonSpawnerRoom;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.BlazingTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.CorrosionTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.CursingTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.DisarmingTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.DisintegrationTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.DistortionTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.FlashingTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.FrostTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.GrimTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.GuardianTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.PitfallTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.RockfallTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.StormTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.SummoningTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.WarpingTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.WeakeningTrap;
+import com.elementalpixel.elementalpixeldungeon.messages.Messages;
+import com.elementalpixel.elementalpixeldungeon.tiles.DungeonTilemap;
+import com.watabou.glwrap.Blending;
+import com.watabou.noosa.Game;
+import com.watabou.noosa.Group;
+import com.watabou.noosa.particles.PixelParticle;
+import com.watabou.utils.PointF;
+import com.watabou.utils.Random;
+
+import java.util.ArrayList;
+
+public class HallsLevel extends RegularLevel {
+
+	{
+		
+		viewDistance = Math.min( 26 - Dungeon.depth, viewDistance );
+		
+		color1 = 0x801500;
+		color2 = 0xa68521;
+	}
+
+	@Override
+	protected ArrayList<Room> initRooms() {
+		ArrayList<Room> rooms = super.initRooms();
+
+		rooms.add(new DemonSpawnerRoom());
+
+		return rooms;
+	}
+
+	@Override
+	public int nMobs() {
+		//remove one mob to account for ripper demon spawners
+		return super.nMobs()-1;
+	}
+
+	@Override
+	protected int standardRooms(boolean forceMax) {
+		if (forceMax) return 10;
+		//8 to 10, average 8.67
+		return 8+ Random.chances(new float[]{3, 2, 1});
+	}
+	
+	@Override
+	protected int specialRooms(boolean forceMax) {
+		if (forceMax) return 3;
+		//2 to 3, average 2.5
+		return 2 + Random.chances(new float[]{1, 1});
+	}
+	
+	@Override
+	protected Painter painter() {
+		return new HallsPainter()
+				.setWater(feeling == Feeling.WATER ? 0.70f : 0.15f, 6)
+				.setGrass(feeling == Feeling.GRASS ? 0.65f : 0.10f, 3)
+				.setTraps(nTraps(), trapClasses(), trapChances());
+	}
+	
+	@Override
+	public void create() {
+		addItemToSpawn( new Torch() );
+		addItemToSpawn( new Torch() );
+		super.create();
+	}
+	
+	@Override
+	public String tilesTex() {
+		return Assets.Environment.TILES_HALLS;
+	}
+	
+	@Override
+	public String waterTex() {
+		return Assets.Environment.WATER_HALLS;
+	}
+	
+	@Override
+	protected Class<?>[] trapClasses() {
+		return new Class[]{
+				FrostTrap.class, StormTrap.class, CorrosionTrap.class, BlazingTrap.class, DisintegrationTrap.class,
+				RockfallTrap.class, FlashingTrap.class, GuardianTrap.class, WeakeningTrap.class,
+				DisarmingTrap.class, SummoningTrap.class, WarpingTrap.class, CursingTrap.class, GrimTrap.class, PitfallTrap.class, DistortionTrap.class };
+	}
+
+	@Override
+	protected float[] trapChances() {
+		return new float[]{
+				4, 4, 4, 4, 4,
+				2, 2, 2, 2,
+				1, 1, 1, 1, 1, 1, 1 };
+	}
+	
+	@Override
+	public String tileName( int tile ) {
+		switch (tile) {
+			case Terrain.WATER:
+				return Messages.get(HallsLevel.class, "water_name");
+			case Terrain.GRASS:
+				return Messages.get(HallsLevel.class, "grass_name");
+			case Terrain.HIGH_GRASS:
+				return Messages.get(HallsLevel.class, "high_grass_name");
+			case Terrain.STATUE:
+			case Terrain.STATUE_SP:
+				return Messages.get(HallsLevel.class, "statue_name");
+			default:
+				return super.tileName( tile );
+		}
+	}
+	
+	@Override
+	public String tileDesc(int tile) {
+		switch (tile) {
+			case Terrain.WATER:
+				return Messages.get(HallsLevel.class, "water_desc");
+			case Terrain.STATUE:
+			case Terrain.STATUE_SP:
+				return Messages.get(HallsLevel.class, "statue_desc");
+			case Terrain.BOOKSHELF:
+				return Messages.get(HallsLevel.class, "bookshelf_desc");
+			default:
+				return super.tileDesc( tile );
+		}
+	}
+	
+	@Override
+	public Group addVisuals() {
+		super.addVisuals();
+		addHallsVisuals( this, visuals );
+		return visuals;
+	}
+	
+	public static void addHallsVisuals( Level level, Group group ) {
+		for (int i=0; i < level.length(); i++) {
+			if (level.map[i] == Terrain.WATER) {
+				group.add( new Stream( i ) );
+			}
+		}
+	}
+	
+	private static class Stream extends Group {
+		
+		private int pos;
+		
+		private float delay;
+		
+		public Stream( int pos ) {
+			super();
+			
+			this.pos = pos;
+			
+			delay = Random.Float( 2 );
+		}
+		
+		@Override
+		public void update() {
+
+			if (!Dungeon.level.water[pos]){
+				killAndErase();
+				return;
+			}
+			
+			if (visible = (pos < Dungeon.level.heroFOV.length && Dungeon.level.heroFOV[pos])) {
+				
+				super.update();
+				
+				if ((delay -= Game.elapsed) <= 0) {
+					
+					delay = Random.Float( 2 );
+					
+					PointF p = DungeonTilemap.tileToWorld( pos );
+					((FireParticle)recycle( FireParticle.class )).reset(
+						p.x + Random.Float( DungeonTilemap.SIZE ),
+						p.y + Random.Float( DungeonTilemap.SIZE ) );
+				}
+			}
+		}
+		
+		@Override
+		public void draw() {
+			Blending.setLightMode();
+			super.draw();
+			Blending.setNormalMode();
+		}
+	}
+	
+	public static class FireParticle extends PixelParticle.Shrinking {
+		
+		public FireParticle() {
+			super();
+			
+			color( 0xEE7722 );
+			lifespan = 1f;
+			
+			acc.set( 0, +80 );
+		}
+		
+		public void reset( float x, float y ) {
+			revive();
+			
+			this.x = x;
+			this.y = y;
+			
+			left = lifespan;
+			
+			speed.set( 0, -40 );
+			size = 4;
+		}
+		
+		@Override
+		public void update() {
+			super.update();
+			float p = left / lifespan;
+			am = p > 0.8f ? (1 - p) * 5 : 1;
+		}
+	}
+}
---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/levels/SewerLevel.java
@@ -0,0 +1,226 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.levels;
+
+
+import com.elementalpixel.elementalpixeldungeon.Assets;
+import com.elementalpixel.elementalpixeldungeon.Dungeon;
+import com.elementalpixel.elementalpixeldungeon.actors.mobs.npcs.Ghost;
+import com.elementalpixel.elementalpixeldungeon.effects.Ripple;
+import com.elementalpixel.elementalpixeldungeon.items.DewVial;
+import com.elementalpixel.elementalpixeldungeon.levels.painters.Painter;
+import com.elementalpixel.elementalpixeldungeon.levels.painters.SewerPainter;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.AlarmTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.ChillingTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.ConfusionTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.FlockTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.OozeTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.ShockingTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.SummoningTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.TeleportationTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.ToxicTrap;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.WornDartTrap;
+import com.elementalpixel.elementalpixeldungeon.messages.Messages;
+import com.elementalpixel.elementalpixeldungeon.scenes.GameScene;
+import com.elementalpixel.elementalpixeldungeon.tiles.DungeonTilemap;
+import com.watabou.noosa.Game;
+import com.watabou.noosa.Group;
+import com.watabou.noosa.particles.Emitter;
+import com.watabou.noosa.particles.PixelParticle;
+import com.watabou.utils.ColorMath;
+import com.watabou.utils.PointF;
+import com.watabou.utils.Random;
+
+public class SewerLevel extends RegularLevel {
+
+	{
+		color1 = 0x48763c;
+		color2 = 0x59994a;
+	}
+	
+	@Override
+	protected int standardRooms(boolean forceMax) {
+		if (forceMax) return 7;
+		//5 to 7, average 5.57
+		return 5+ Random.chances(new float[]{4, 2, 1});
+	}
+	
+	@Override
+	protected int specialRooms(boolean forceMax) {
+		if (forceMax) return 3;
+		//1 to 3, average 1.8
+		return 1+Random.chances(new float[]{4, 4, 2});
+	}
+	
+	@Override
+	protected Painter painter() {
+		return new SewerPainter()
+				.setWater(feeling == Feeling.WATER ? 0.85f : 0.30f, 5)
+				.setGrass(feeling == Feeling.GRASS ? 0.80f : 0.20f, 4)
+				.setTraps(nTraps(), trapClasses(), trapChances());
+	}
+	
+	@Override
+	public String tilesTex() {
+		return Assets.Environment.TILES_SEWERS;
+	}
+	
+	@Override
+	public String waterTex() {
+		return Assets.Environment.WATER_SEWERS;
+	}
+	
+	@Override
+	protected Class<?>[] trapClasses() {
+		return Dungeon.depth == 1 ?
+				new Class<?>[]{ WornDartTrap.class } :
+				new Class<?>[]{
+						ChillingTrap.class, ShockingTrap.class, ToxicTrap.class, WornDartTrap.class,
+						AlarmTrap.class, OozeTrap.class,
+						ConfusionTrap.class, FlockTrap.class, SummoningTrap.class, TeleportationTrap.class };
+}
+
+	@Override
+	protected float[] trapChances() {
+		return Dungeon.depth == 1 ?
+				new float[]{1} :
+				new float[]{
+						4, 4, 4, 4,
+						2, 2,
+						1, 1, 1, 1};
+	}
+	
+	@Override
+	protected void createItems() {
+		if (!Dungeon.LimitedDrops.DEW_VIAL.dropped()) {
+			addItemToSpawn( new DewVial() );
+			Dungeon.LimitedDrops.DEW_VIAL.drop();
+		}
+
+		Ghost.Quest.spawn( this );
+		
+		super.createItems();
+	}
+	
+	@Override
+	public Group addVisuals() {
+		super.addVisuals();
+		addSewerVisuals(this, visuals);
+		return visuals;
+	}
+	
+	public static void addSewerVisuals( Level level, Group group ) {
+		for (int i=0; i < level.length(); i++) {
+			if (level.map[i] == Terrain.WALL_DECO) {
+				group.add( new Sink( i ) );
+			}
+		}
+	}
+	
+	@Override
+	public String tileName( int tile ) {
+		switch (tile) {
+			case Terrain.WATER:
+				return Messages.get(SewerLevel.class, "water_name");
+			default:
+				return super.tileName( tile );
+		}
+	}
+	
+	@Override
+	public String tileDesc(int tile) {
+		switch (tile) {
+			case Terrain.EMPTY_DECO:
+				return Messages.get(SewerLevel.class, "empty_deco_desc");
+			case Terrain.BOOKSHELF:
+				return Messages.get(SewerLevel.class, "bookshelf_desc");
+			default:
+				return super.tileDesc( tile );
+		}
+	}
+	
+	private static class Sink extends Emitter {
+		
+		private int pos;
+		private float rippleDelay = 0;
+		
+		private static final Emitter.Factory factory = new Factory() {
+			
+			@Override
+			public void emit( Emitter emitter, int index, float x, float y ) {
+				WaterParticle p = (WaterParticle)emitter.recycle( WaterParticle.class );
+				p.reset( x, y );
+			}
+		};
+		
+		public Sink( int pos ) {
+			super();
+			
+			this.pos = pos;
+			
+			PointF p = DungeonTilemap.tileCenterToWorld( pos );
+			pos( p.x - 2, p.y + 3, 4, 0 );
+			
+			pour( factory, 0.1f );
+		}
+		
+		@Override
+		public void update() {
+			if (visible = (pos < Dungeon.level.heroFOV.length && Dungeon.level.heroFOV[pos])) {
+				
+				super.update();
+				
+				if (!isFrozen() && (rippleDelay -= Game.elapsed) <= 0) {
+					Ripple ripple = GameScene.ripple( pos + Dungeon.level.width() );
+					if (ripple != null) {
+						ripple.y -= DungeonTilemap.SIZE / 2;
+						rippleDelay = Random.Float(0.4f, 0.6f);
+					}
+				}
+			}
+		}
+	}
+	
+	public static final class WaterParticle extends PixelParticle {
+		
+		public WaterParticle() {
+			super();
+			
+			acc.y = 50;
+			am = 0.5f;
+			
+			color( ColorMath.random( 0xb6ccc2, 0x3b6653 ) );
+			size( 2 );
+		}
+		
+		public void reset( float x, float y ) {
+			revive();
+			
+			this.x = x;
+			this.y = y;
+			
+			speed.set( Random.Float( -2, +2 ), 0 );
+			
+			left = lifespan = 0.4f;
+		}
+	}
+}
