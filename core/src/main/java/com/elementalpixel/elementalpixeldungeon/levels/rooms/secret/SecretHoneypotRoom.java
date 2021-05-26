9bccba3eebb84762ad85de38c05253e593db5d85
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 19:16:33 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/levels/rooms/secret/SecretHoneypotRoom.java
@@ -0,0 +1,73 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.levels.rooms.secret;
+
+
+import com.elementalpixel.elementalpixeldungeon.Dungeon;
+import com.elementalpixel.elementalpixeldungeon.actors.mobs.Bee;
+import com.elementalpixel.elementalpixeldungeon.items.Honeypot;
+import com.elementalpixel.elementalpixeldungeon.items.Item;
+import com.elementalpixel.elementalpixeldungeon.items.bombs.Bomb;
+import com.elementalpixel.elementalpixeldungeon.levels.Level;
+import com.elementalpixel.elementalpixeldungeon.levels.Terrain;
+import com.elementalpixel.elementalpixeldungeon.levels.painters.Painter;
+import com.watabou.utils.Point;
+
+public class SecretHoneypotRoom extends SecretRoom {
+	
+	@Override
+	public void paint(Level level) {
+		Painter.fill( level, this, Terrain.WALL );
+		Painter.fill(level, this, 1, Terrain.EMPTY );
+		
+		Point brokenPotPos = center();
+		
+		brokenPotPos.x = (brokenPotPos.x + entrance().x) / 2;
+		brokenPotPos.y = (brokenPotPos.y + entrance().y) / 2;
+		
+		Honeypot.ShatteredPot pot = new Honeypot.ShatteredPot();
+		level.drop(pot, level.pointToCell(brokenPotPos));
+		
+		Bee bee = new Bee();
+		bee.spawn( Dungeon.depth );
+		bee.HP = bee.HT;
+		bee.pos = level.pointToCell(brokenPotPos);
+		level.mobs.add( bee );
+		
+		bee.setPotInfo(level.pointToCell(brokenPotPos), null);
+		
+		placeItem(new Honeypot(), level);
+		
+		placeItem( new Bomb().random(), level);
+		
+		entrance().set(Door.Type.HIDDEN);
+	}
+	
+	private void placeItem(Item item, Level level){
+		int itemPos;
+		do {
+			itemPos = level.pointToCell(random());
+		} while (level.heaps.get(itemPos) != null);
+		
+		level.drop(item, itemPos);
+	}
+}
---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/levels/rooms/secret/SecretSummoningRoom.java
@@ -0,0 +1,65 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.levels.rooms.secret;
+
+
+import com.elementalpixel.elementalpixeldungeon.items.Generator;
+import com.elementalpixel.elementalpixeldungeon.items.Heap;
+import com.elementalpixel.elementalpixeldungeon.levels.Level;
+import com.elementalpixel.elementalpixeldungeon.levels.Terrain;
+import com.elementalpixel.elementalpixeldungeon.levels.painters.Painter;
+import com.elementalpixel.elementalpixeldungeon.levels.traps.SummoningTrap;
+import com.watabou.utils.Point;
+
+public class SecretSummoningRoom extends SecretRoom {
+	
+	//minimum of 3x3 traps, max of 6x6 traps
+	
+	@Override
+	public int maxWidth() {
+		return 8;
+	}
+	
+	@Override
+	public int maxHeight() {
+		return 8;
+	}
+	
+	@Override
+	public void paint(Level level) {
+		Painter.fill(level, this, Terrain.WALL);
+		Painter.fill(level, this, 1, Terrain.SECRET_TRAP);
+		
+		Point center = center();
+		level.drop(Generator.random(), level.pointToCell(center)).setHauntedIfCursed().type = Heap.Type.SKELETON;
+		
+		for (Point p : getPoints()){
+			int cell = level.pointToCell(p);
+			if (level.map[cell] == Terrain.SECRET_TRAP){
+				level.setTrap(new SummoningTrap().hide(), cell);
+			}
+		}
+		
+		entrance().set(Door.Type.HIDDEN);
+	}
+	
+}
