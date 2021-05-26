f79c7daf8e55e50131bda703c6038e578102c64a
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 18:42:28 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/actors/mobs/npcs/Shopkeeper.java
@@ -0,0 +1,142 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.actors.mobs.npcs;
+
+
+import com.elementalpixel.elementalpixeldungeon.Dungeon;
+import com.elementalpixel.elementalpixeldungeon.actors.Char;
+import com.elementalpixel.elementalpixeldungeon.actors.buffs.Buff;
+import com.elementalpixel.elementalpixeldungeon.effects.CellEmitter;
+import com.elementalpixel.elementalpixeldungeon.effects.particles.ElmoParticle;
+import com.elementalpixel.elementalpixeldungeon.items.Heap;
+import com.elementalpixel.elementalpixeldungeon.items.Item;
+import com.elementalpixel.elementalpixeldungeon.items.armor.Armor;
+import com.elementalpixel.elementalpixeldungeon.journal.Notes;
+import com.elementalpixel.elementalpixeldungeon.messages.Messages;
+import com.elementalpixel.elementalpixeldungeon.scenes.GameScene;
+import com.elementalpixel.elementalpixeldungeon.sprites.ShopkeeperSprite;
+import com.elementalpixel.elementalpixeldungeon.windows.WndBag;
+import com.elementalpixel.elementalpixeldungeon.windows.WndTradeItem;
+import com.watabou.noosa.Game;
+import com.watabou.utils.Callback;
+
+public class Shopkeeper extends NPC {
+
+	{
+		spriteClass = ShopkeeperSprite.class;
+
+		properties.add(Property.IMMOVABLE);
+	}
+	
+	@Override
+	protected boolean act() {
+
+		if (Dungeon.level.heroFOV[pos]){
+			Notes.add(Notes.Landmark.SHOP);
+		}
+		
+		sprite.turnTo( pos, Dungeon.hero.pos );
+		spend( TICK );
+		return super.act();
+	}
+	
+	@Override
+	public void damage( int dmg, Object src ) {
+		flee();
+	}
+	
+	@Override
+	public void add( Buff buff ) {
+		flee();
+	}
+	
+	public void flee() {
+		destroy();
+
+		Notes.remove(Notes.Landmark.SHOP);
+		
+		sprite.killAndErase();
+		CellEmitter.get( pos ).burst( ElmoParticle.FACTORY, 6 );
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		for (Heap heap: Dungeon.level.heaps.valueList()) {
+			if (heap.type == Heap.Type.FOR_SALE) {
+				CellEmitter.get( heap.pos ).burst( ElmoParticle.FACTORY, 4 );
+				if (heap.size() == 1) {
+					heap.destroy();
+				} else {
+					heap.items.remove(heap.size()-1);
+					heap.type = Heap.Type.HEAP;
+				}
+			}
+		}
+	}
+	
+	@Override
+	public boolean reset() {
+		return true;
+	}
+
+	//shopkeepers are greedy!
+	public static int sellPrice(Item item){
+		return item.value() * 5 * (Dungeon.depth / 5 + 1);
+	}
+	
+	public static WndBag sell() {
+		return GameScene.selectItem( itemSelector, WndBag.Mode.FOR_SALE, Messages.get(Shopkeeper.class, "sell"));
+	}
+
+	public static boolean willBuyItem( Item item ){
+		if (item.value() <= 0)                                               return false;
+		if (item.unique && !item.stackable)                                 return false;
+		if (item instanceof Armor && ((Armor) item).checkSeal() != null)    return false;
+		if (item.isEquipped(Dungeon.hero) && item.cursed)                   return false;
+		return true;
+	}
+	
+	private static WndBag.Listener itemSelector = new WndBag.Listener() {
+		@Override
+		public void onSelect( Item item ) {
+			if (item != null) {
+				WndBag parentWnd = sell();
+				GameScene.show( new WndTradeItem( item, parentWnd ) );
+			}
+		}
+	};
+
+	@Override
+	public boolean interact(Char c) {
+		if (c != Dungeon.hero) {
+			return true;
+		}
+		Game.runOnRenderThread(new Callback() {
+			@Override
+			public void call() {
+				sell();
+			}
+		});
+		return true;
+	}
+}
