0d74d9cd1fee7b9a164dd279b4c90592372af2b4
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 19:05:37 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/weapon/enchantments/Corrupting.java
@@ -0,0 +1,98 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.items.weapon.enchantments;
+
+
+import com.elementalpixel.elementalpixeldungeon.Badges;
+import com.elementalpixel.elementalpixeldungeon.Dungeon;
+import com.elementalpixel.elementalpixeldungeon.Statistics;
+import com.elementalpixel.elementalpixeldungeon.actors.Char;
+import com.elementalpixel.elementalpixeldungeon.actors.buffs.Buff;
+import com.elementalpixel.elementalpixeldungeon.actors.buffs.Corruption;
+import com.elementalpixel.elementalpixeldungeon.actors.buffs.PinCushion;
+import com.elementalpixel.elementalpixeldungeon.actors.buffs.SoulMark;
+import com.elementalpixel.elementalpixeldungeon.actors.hero.Hero;
+import com.elementalpixel.elementalpixeldungeon.actors.mobs.Mob;
+import com.elementalpixel.elementalpixeldungeon.items.weapon.Weapon;
+import com.elementalpixel.elementalpixeldungeon.messages.Messages;
+import com.elementalpixel.elementalpixeldungeon.sprites.CharSprite;
+import com.elementalpixel.elementalpixeldungeon.sprites.ItemSprite;
+import com.watabou.utils.Random;
+
+public class Corrupting extends Weapon.Enchantment {
+	
+	private static ItemSprite.Glowing BLACK = new ItemSprite.Glowing( 0x440066 );
+	
+	@Override
+	public int proc(Weapon weapon, Char attacker, Char defender, int damage) {
+		int level = Math.max( 0, weapon.buffedLvl() );
+		
+		// lvl 0 - 20%
+		// lvl 1 ~ 23%
+		// lvl 2 ~ 26%
+		float procChance = (level+5f)/(level+25f) * procChanceMultiplier(attacker);
+		if (damage >= defender.HP
+				&& Random.Float() < procChance
+				&& !defender.isImmune(Corruption.class)
+				&& defender.buff(Corruption.class) == null
+				&& defender instanceof Mob
+				&& defender.isAlive()){
+			
+			Mob enemy = (Mob) defender;
+			Hero hero = (attacker instanceof Hero) ? (Hero) attacker : Dungeon.hero;
+			
+			enemy.HP = enemy.HT;
+			for (Buff buff : enemy.buffs()) {
+				if (buff.type == Buff.buffType.NEGATIVE
+						&& !(buff instanceof SoulMark)) {
+					buff.detach();
+				} else if (buff instanceof PinCushion){
+					buff.detach();
+				}
+			}
+			if (enemy.alignment == Char.Alignment.ENEMY){
+				enemy.rollToDropLoot();
+			}
+			
+			Buff.affect(enemy, Corruption.class);
+			
+			Statistics.enemiesSlain++;
+			Badges.validateMonstersSlain();
+			Statistics.qualifiedForNoKilling = false;
+			if (enemy.EXP > 0 && hero.lvl <= enemy.maxLvl) {
+				hero.sprite.showStatus(CharSprite.POSITIVE, Messages.get(enemy, "exp", enemy.EXP));
+				hero.earnExp(enemy.EXP, enemy.getClass());
+			} else {
+				hero.earnExp(0, enemy.getClass());
+			}
+			
+			return 0;
+		}
+		
+		return damage;
+	}
+	
+	@Override
+	public ItemSprite.Glowing glowing() {
+		return BLACK;
+	}
+}
