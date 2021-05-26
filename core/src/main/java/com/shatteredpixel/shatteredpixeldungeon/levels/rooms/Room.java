9764c16e571a27eb120a703ed10a9ea465522572
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 15:48:46 +0200

    Add files via upload

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/Room.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/Room.java
@@ -19,9 +19,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-package com.shatteredpixel.shatteredpixeldungeon.levels.rooms;
+package com.elementalpixel.elementalpixeldungeon.levels.rooms;
 
-import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
+
+import com.elementalpixel.elementalpixeldungeon.levels.Level;
 import com.watabou.utils.Bundlable;
 import com.watabou.utils.Bundle;
 import com.watabou.utils.Graph;
