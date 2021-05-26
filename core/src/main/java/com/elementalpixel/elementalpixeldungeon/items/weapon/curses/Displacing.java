0b275aeae06071612ba6ee31d7b3bcf137b55cd8
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 19:04:53 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/weapon/curses/Displacing.java
@@ -0,0 +1,83 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.items.weapon.curses;
+
+
+import com.elementalpixel.elementalpixeldungeon.Dungeon;
+import com.elementalpixel.elementalpixeldungeon.actors.Char;
+import com.elementalpixel.elementalpixeldungeon.actors.mobs.Mob;
+import com.elementalpixel.elementalpixeldungeon.effects.CellEmitter;
+import com.elementalpixel.elementalpixeldungeon.effects.Speck;
+import com.elementalpixel.elementalpixeldungeon.items.weapon.Weapon;
+import com.elementalpixel.elementalpixeldungeon.sprites.ItemSprite;
+import com.watabou.utils.Random;
+
+public class Displacing extends Weapon.Enchantment {
+
+	private static ItemSprite.Glowing BLACK = new ItemSprite.Glowing( 0x000000 );
+
+	@Override
+	public int proc(Weapon weapon, Char attacker, Char defender, int damage ) {
+
+		float procChance = 1/12f * procChanceMultiplier(attacker);
+		if (Random.Float() < procChance && !defender.properties().contains(Char.Property.IMMOVABLE)){
+			int count = 20;
+			int newPos;
+			do {
+				newPos = Dungeon.level.randomRespawnCell( defender );
+				if (count-- <= 0) {
+					break;
+				}
+			} while (newPos == -1 || Dungeon.level.secret[newPos]);
+
+			if (newPos != -1 && !Dungeon.bossLevel()) {
+
+				if (Dungeon.level.heroFOV[defender.pos]) {
+					CellEmitter.get( defender.pos ).start( Speck.factory( Speck.LIGHT ), 0.2f, 3 );
+				}
+
+				defender.pos = newPos;
+				if (defender instanceof Mob && ((Mob) defender).state == ((Mob) defender).HUNTING){
+					((Mob) defender).state = ((Mob) defender).WANDERING;
+				}
+				defender.sprite.place( defender.pos );
+				defender.sprite.visible = Dungeon.level.heroFOV[defender.pos];
+
+				return 0;
+
+			}
+		}
+
+		return damage;
+	}
+
+	@Override
+	public boolean curse() {
+		return true;
+	}
+
+	@Override
+	public ItemSprite.Glowing glowing() {
+		return BLACK;
+	}
+
+}
