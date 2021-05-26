34d8624462c00f79e53590a769e36590f930ba25
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 20:56:04 +0200

    Update Assets.java

--- core/src/main/java/com/elementalpixel/elementalpixeldungeon/Assets.java
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/Assets.java
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-package com.elementalpixel.elementalpixeldungeon.items;
+package com.elementalpixel.elementalpixeldungeon;
 
 public class Assets {
 
