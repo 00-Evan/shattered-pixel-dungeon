ab01e499da360611c82ee75d4afac6c4450868a2
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 16:38:33 +0200

    Add files via upload

--- services/updates/githubUpdates/src/main/java/com/shatteredpixel/shatteredpixeldungeon/services/updates/GitHubUpdates.java
+++ services/updates/githubUpdates/src/main/java/com/shatteredpixel/shatteredpixeldungeon/services/updates/GitHubUpdates.java
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-package com.shatteredpixel.shatteredpixeldungeon.services.updates;
+package com.elementalpixel.elementalpixeldungeon.services.updates;
 
 
 import com.badlogic.gdx.Gdx;
