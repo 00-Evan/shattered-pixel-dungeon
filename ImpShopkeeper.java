df133a690383b403384cada8d78ba23b122f5dc3
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 15:22:23 +0200

    Add files via upload

---
+++ ImpShopkeeper.java
@@ -0,0 +1,66 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.actors.mobs.npcs;
+
+
+import com.elementalpixel.elementalpixeldungeon.Dungeon;
+import com.elementalpixel.elementalpixeldungeon.effects.CellEmitter;
+import com.elementalpixel.elementalpixeldungeon.effects.Speck;
+import com.elementalpixel.elementalpixeldungeon.effects.particles.ElmoParticle;
+import com.elementalpixel.elementalpixeldungeon.items.Heap;
+import com.elementalpixel.elementalpixeldungeon.messages.Messages;
+import com.elementalpixel.elementalpixeldungeon.sprites.ImpSprite;
+
+public class ImpShopkeeper extends Shopkeeper {
+
+	{
+		spriteClass = ImpSprite.class;
+	}
+	
+	private boolean seenBefore = false;
+	
+	@Override
+	protected boolean act() {
+
+		if (!seenBefore && Dungeon.level.heroFOV[pos]) {
+			yell( Messages.get(this, "greetings", Dungeon.hero.name() ) );
+			seenBefore = true;
+		}
+		
+		return super.act();
+	}
+	
+	@Override
+	public void flee() {
+		for (Heap heap: Dungeon.level.heaps.valueList()) {
+			if (heap.type == Heap.Type.FOR_SALE) {
+				CellEmitter.get( heap.pos ).burst( ElmoParticle.FACTORY, 4 );
+				heap.destroy();
+			}
+		}
+		
+		destroy();
+		
+		sprite.emitter().burst( Speck.factory( Speck.WOOL ), 15 );
+		sprite.killAndErase();
+	}
+}
