bbf1649a1a3e03c1f891b0e1a049b452e9aa6cc5
Meel703 <81315104+Meel703@users.noreply.github.com>
2021-05-26 19:15:55 +0200

    Add files via upload

---
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/levels/rooms/connection/RingBridgeRoom.java
@@ -0,0 +1,59 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2021 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.elementalpixel.elementalpixeldungeon.levels.rooms.connection;
+
+
+import com.elementalpixel.elementalpixeldungeon.levels.Level;
+import com.elementalpixel.elementalpixeldungeon.levels.Terrain;
+import com.elementalpixel.elementalpixeldungeon.levels.painters.Painter;
+import com.elementalpixel.elementalpixeldungeon.levels.rooms.Room;
+import com.watabou.utils.Point;
+import com.watabou.utils.Rect;
+
+public class RingBridgeRoom extends RingTunnelRoom {
+
+	@Override
+	public void paint(Level level) {
+		Painter.fill(level, this, 1, Terrain.CHASM);
+
+		super.paint(level);
+
+		for (Room r : neigbours){
+			if (r instanceof BridgeRoom || r instanceof RingBridgeRoom || r instanceof WalkwayRoom){
+				Rect i = intersect(r);
+				if (i.width() != 0){
+					i.left++;
+					i.right--;
+				} else {
+					i.top++;
+					i.bottom--;
+				}
+				Painter.fill(level, i.left, i.top, i.width()+1, i.height()+1, Terrain.CHASM);
+			}
+		}
+	}
+
+	@Override
+	public boolean canMerge(Level l, Point p, int mergeTerrain) {
+		return mergeTerrain == Terrain.CHASM;
+	}
+}
