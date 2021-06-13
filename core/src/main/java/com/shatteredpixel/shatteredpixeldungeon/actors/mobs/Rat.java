fc6b08e2f14ee08429c108469282398177a16f15
Evan Debenham <Evan@ShatteredPixel.com>
2021-06-13 14:25:48 -0400

    v0.9.3a: fixed ally rats forgetting ally state on game load

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Rat.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Rat.java
@@ -25,6 +25,7 @@ import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.Ratmogrify;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.RatSprite;
+import com.watabou.utils.Bundle;
 import com.watabou.utils.Random;
 
 public class Rat extends Mob {
@@ -60,5 +61,19 @@ public class Rat extends Mob {
 	@Override
 	public int drRoll() {
 		return Random.NormalIntRange(0, 1);
+	}
+
+	private static final String RAT_ALLY = "rat_ally";
+
+	@Override
+	public void storeInBundle(Bundle bundle) {
+		super.storeInBundle(bundle);
+		if (alignment == Alignment.ALLY) bundle.put(RAT_ALLY, true);
+	}
+
+	@Override
+	public void restoreFromBundle(Bundle bundle) {
+		super.restoreFromBundle(bundle);
+		if (bundle.contains(RAT_ALLY)) alignment = Alignment.ALLY;
 	}
 }
