401e41c82231c2278c2df88ffe8fdbe467478fe5
Meel703 <asebo29@gmail.com>
2021-06-15 19:30:09 +0200

    Added alchemist sprites to Assets.java

--- core/src/main/java/com/elementalpixel/elementalpixeldungeon/Assets.java
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/Assets.java
@@ -211,6 +211,7 @@ public class Assets {
 		public static final String MAGE		= "splashes/mage.jpg";
 		public static final String ROGUE	= "splashes/rogue.jpg";
 		public static final String HUNTRESS	= "splashes/huntress.jpg";
+		public static final String ALCHEMIST= "splashes/alchemist.jpg";
 	}
 
 	public static class Sprites {
@@ -221,6 +222,7 @@ public class Assets {
 		public static final String MAGE		= "sprites/mage.png";
 		public static final String ROGUE	= "sprites/rogue.png";
 		public static final String HUNTRESS	= "sprites/huntress.png";
+		public static final String ALCHEMIST= "sprites/alchemist.png";
 		public static final String AVATARS	= "sprites/avatars.png";
 		public static final String PET		= "sprites/pet.png";
 		public static final String AMULET	= "sprites/amulet.png";
