ef9727a76b93e91bfeac56e6437998647ff51c80
Meel703 <asebo29@gmail.com>
2021-06-16 18:13:46 +0200

    Added some code to fragments

--- core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/fragments/AirFragment.java
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/fragments/AirFragment.java
@@ -1,9 +1,26 @@
 package com.elementalpixel.elementalpixeldungeon.items.fragments;
 
+import com.elementalpixel.elementalpixeldungeon.actors.hero.Hero;
 import com.elementalpixel.elementalpixeldungeon.sprites.ItemSpriteSheet;
 
 public class AirFragment extends Fragment {
     {
         image = ItemSpriteSheet.AIR_FRAGMENT;
+        unique = true;
+        stackable = false;
+        bones = false;
+    }
+
+    public AirFragment(int depth) {
+        super();
+    }
+
+    @Override
+    public boolean doPickUp( Hero hero ) {
+        if (super.doPickUp( hero )) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/fragments/EarthFragment.java
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/items/fragments/EarthFragment.java
@@ -1,9 +1,26 @@
 package com.elementalpixel.elementalpixeldungeon.items.fragments;
 
+import com.elementalpixel.elementalpixeldungeon.actors.hero.Hero;
 import com.elementalpixel.elementalpixeldungeon.sprites.ItemSpriteSheet;
 
 public class EarthFragment extends Fragment {
     {
         image = ItemSpriteSheet.EARTH_FRAGMENT;
+        unique = true;
+        stackable = false;
+        bones = false;
+    }
+
+    public EarthFragment(int depth) {
+        super();
+    }
+
+    @Override
+    public boolean doPickUp( Hero hero ) {
+        if (super.doPickUp( hero )) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
