cd8abf720c9479d635e412861c0e2ad69075f739
Evan Debenham <Evan@ShatteredPixel.com>
2021-06-22 18:44:01 -0400

    v0.9.3c: final commit

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/v0_9_X_Changes.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/v0_9_X_Changes.java
@@ -61,15 +61,39 @@ public class v0_9_X_Changes {
 		changes.hardlight(Window.TITLE_COLOR);
 		changeInfos.add(changes);
 
-		changes = new ChangeInfo("v0.9.3b", false, null);
+		changes = new ChangeInfo("", false, null);
+		changes.hardlight(Window.TITLE_COLOR);
+		changeInfos.add(changes);
+
+		changes = new ChangeInfo("v0.9.3c", false, null);
 		changes.hardlight(Window.TITLE_COLOR);
 		changeInfos.add(changes);
 
 		if (Gdx.app.getType() == Application.ApplicationType.Android && Gdx.app.getVersion() < 14) {
 			changes.addButton(new ChangeButton(Icons.get(Icons.WARNING), "Android 2.3 and 3 Support",
-					"v0.9.3b is the final version of Shattered Pixel Dungeon which supports Android 2.3 Gingerbread and Android 3 Honeycomb. I don't currently have plans to end support for other Android versions.\n\n" +
+					"v0.9.3c is the final version of Shattered Pixel Dungeon which supports Android 2.3 Gingerbread and Android 3 Honeycomb. I don't currently have plans to end support for other Android versions.\n\n" +
 					"If you'd like to keep up to date on future updates, check out the blog at _ShatteredPixel.com_"));
 		}
+
+		changes.addButton(new ChangeButton(Icons.get(Icons.PREFS), Messages.get(ChangesScene.class, "misc"),
+				"_-_ The hero can now collect dew on stairs, even if they are at full HP.\n\n" +
+				"_-_ In the Google Play version of the game, the player can now separately enable Google Play Games and opt-out of sharing gameplay data."));
+
+		changes.addButton(new ChangeButton(new Image(Assets.Sprites.SPINNER, 144, 0, 16, 16), Messages.get(ChangesScene.class, "bugfixes"),
+				"Fixed (caused by v0.9.3):\n" +
+				"_-_ Various errors with warp beacon\n" +
+				"_-_ Quick use window being usable after hero death\n" +
+				"_-_ Rare cases of rankings failing to load\n" +
+				"_-_ Fear from death mark lasting far longer than intended\n" +
+				"_-_ Errors with the Lightweight Cloak talent\n\n" +
+				"Fixed (existed prior to v0.9.3):\n" +
+				"_-_ Necromancers sometimes losing track of their skeleton\n" +
+				"_-_ Deaths to Dwarf King rarely not being recorded in rankings\n" +
+				"_-_ Various minor text errors"));
+
+		changes = new ChangeInfo("v0.9.3b", false, null);
+		changes.hardlight(Window.TITLE_COLOR);
+		changeInfos.add(changes);
 
 		changes.addButton(new ChangeButton(Icons.get(Icons.PREFS), Messages.get(ChangesScene.class, "misc"),
 				"It is now possible to access a quick-use window by quickslotting a bag or by long-pressing the inventory! A few items that were missing quick actions have also been given them, and bags can now appear in the rankings.\n\n" +
