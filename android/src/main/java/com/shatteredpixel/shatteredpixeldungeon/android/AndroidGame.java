4df6e1ec0d157f7643309a601266041c08f6ab68
Evan Debenham <Evan@ShatteredPixel.com>
2021-06-25 16:43:03 -0400

    v0.9.4: moved to async android audio, added in gdx 1.9.12

--- android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidGame.java
+++ android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidGame.java
@@ -21,6 +21,7 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.android;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -30,6 +31,8 @@ import android.view.ViewConfiguration;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.AndroidAudio;
+import com.badlogic.gdx.backends.android.AsynchronousAndroidAudio;
 import com.shatteredpixel.shatteredpixeldungeon.SPDSettings;
 import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
 import com.shatteredpixel.shatteredpixeldungeon.services.news.News;
@@ -113,6 +116,11 @@ public class AndroidGame extends AndroidApplication {
 		
 		initialize(new ShatteredPixelDungeon(support), config);
 		
+	}
+
+	@Override
+	public AndroidAudio createAudio(Context context, AndroidApplicationConfiguration config) {
+		return new AsynchronousAndroidAudio(context, config);
 	}
 
 	@Override
