5af8dff38ff2f7ebe02c32bff5c079611506db26
saqfish <saqfish@gmail.com>
2021-07-17 01:32:27 -0500

    add host/key entering

--- core/src/main/java/com/saqfish/spdnet/net/ui/NetBtn.java
+++ core/src/main/java/com/saqfish/spdnet/net/ui/NetBtn.java
@@ -2,8 +2,16 @@ package com.saqfish.spdnet.net.ui;
 
 import com.saqfish.spdnet.Chrome;
 import com.saqfish.spdnet.ShatteredPixelDungeon;
+import com.saqfish.spdnet.net.Settings;
 import com.saqfish.spdnet.net.windows.NetWindow;
 import com.saqfish.spdnet.ui.StyledButton;
+import com.watabou.utils.PlatformSupport;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static com.saqfish.spdnet.ShatteredPixelDungeon.net;
+import static com.watabou.noosa.Game.platform;
 
 public class NetBtn extends StyledButton {
     public static final int HEIGHT = 24;
@@ -26,5 +34,34 @@ public class NetBtn extends StyledButton {
     protected void onClick() {
         super.onClick();
         NetWindow.showServerInfo();
+    }
+
+    @Override
+    protected boolean onLongClick() {
+        platform.promptTextInput("Enter host", Settings.uri().toString(), 40, false, "Set", "Cancel", new PlatformSupport.TextCallback() {
+            @Override
+            public void onSelect(boolean positive, String text) {
+                if(positive){
+                    URI url = null;
+                    try {
+                        url = new URI(text);
+                        Settings.scheme(url.getScheme());
+                        Settings.address(url.getHost());
+                        Settings.port(url.getPort());
+                        platform.promptTextInput("Enter key", Settings.auth_key(), 20, false, "Set", "Cancel", new PlatformSupport.TextCallback() {
+                            @Override
+                            public void onSelect(boolean positive, String text) {
+                                if(positive){
+                                    Settings.auth_key(text);
+                                    net().reset();
+                                }
+                            }
+                        });
+                    } catch (URISyntaxException e) {
+                    }
+                }
+            }
+        });
+        return true;
     }
 }
