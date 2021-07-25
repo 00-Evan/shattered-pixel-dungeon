b32bfa82f576dc851466d4b600974984b44ee9e6
saqfish <saqfish@gmail.com>
2021-07-24 23:52:51 -0500

    version & unlock

--- SPD-classes/src/main/java/com/watabou/utils/DeviceCompat.java
+++ SPD-classes/src/main/java/com/watabou/utils/DeviceCompat.java
@@ -60,7 +60,8 @@ public class DeviceCompat {
 	}
 	
 	public static boolean isDebug(){
-		return Game.version.contains("INDEV");
+		// return Game.version.contains("INDEV");
+		return true;
 	}
 	
 	public static void openURI( String URI ){
