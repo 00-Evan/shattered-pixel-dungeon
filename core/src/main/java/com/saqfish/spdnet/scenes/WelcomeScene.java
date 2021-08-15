c354ad9bd896ab09aaa6a115b5fe0d83bc74fad9
saqfish <saqfish@gmail.com>
2021-08-15 04:06:20 -0500

    version 5.2

--- core/src/main/java/com/saqfish/spdnet/scenes/WelcomeScene.java
+++ core/src/main/java/com/saqfish/spdnet/scenes/WelcomeScene.java
@@ -143,9 +143,8 @@ public class WelcomeScene extends PixelScene {
 				//TODO: change the messages here in accordance with the type of patch.
 				message = "SPDNet has been patched!";
 				message += "\n";
-				message += "\n Auto reconnect implemented";
-				message += "\n Player list scroll fix";
-				message += "\n Shared items not stacking fix";
+				message += "\n Rankings fix";
+				message += "\n Save on final ascend fix";
 
 			}
 		} else {
