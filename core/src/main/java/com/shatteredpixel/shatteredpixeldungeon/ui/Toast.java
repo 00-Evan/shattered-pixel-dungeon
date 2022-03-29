db6559497d959ef0fdab45941cf75deb0a0c5816
Evan Debenham <Evan@ShatteredPixel.com>
2022-03-28 22:45:56 -0400

    v1.2.1: cell selector prompts are not properly closed with the back key

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/Toast.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/Toast.java
@@ -23,6 +23,7 @@ package com.shatteredpixel.shatteredpixeldungeon.ui;
 
 import com.shatteredpixel.shatteredpixeldungeon.Chrome;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
+import com.watabou.input.GameAction;
 import com.watabou.noosa.NinePatch;
 import com.watabou.noosa.ui.Component;
 
@@ -32,7 +33,7 @@ public class Toast extends Component {
 	private static final float MARGIN_VER	= 2;
 
 	protected NinePatch bg;
-	protected SimpleButton close;
+	protected IconButton close;
 	protected RenderedTextBlock text;
 
 	public Toast( String text ) {
@@ -50,11 +51,17 @@ public class Toast extends Component {
 		bg = Chrome.get( Chrome.Type.TOAST_TR );
 		add( bg );
 
-		close = new SimpleButton( Icons.get( Icons.CLOSE ) ) {
+		close = new IconButton( Icons.get( Icons.CLOSE ) ) {
 			protected void onClick() {
 				onClose();
 			}
+
+			@Override
+			public GameAction keyAction() {
+				return GameAction.BACK;
+			}
 		};
+		close.setSize(close.icon.width(), close.icon.height());
 		add( close );
 
 		text = PixelScene.renderTextBlock(8);
