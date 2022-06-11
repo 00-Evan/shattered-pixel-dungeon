a0bfd8e71f8fcd87216b8975b538a6f1c85c8beb
Evan Debenham <Evan@ShatteredPixel.com>
2022-06-10 22:20:17 -0400

    v1.3.0: fixed additional source merging errors

--- android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
+++ android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
@@ -23,6 +23,7 @@ package com.shatteredpixel.shatteredpixeldungeon.android;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
