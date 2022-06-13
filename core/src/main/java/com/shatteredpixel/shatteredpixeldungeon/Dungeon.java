9e4e7cfa2cc81d3fbfb086737c06e051aaccf8f3
Evan Debenham <Evan@ShatteredPixel.com>
2022-06-13 17:39:40 -0400

    v1.3.0: fixed Goo's level been counted for exploration bonus

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -780,7 +780,7 @@ public class Dungeon {
 	}
 
 	public static void updateLevelExplored(){
-		if (branch == 0 && level instanceof RegularLevel){
+		if (branch == 0 && level instanceof RegularLevel && !Dungeon.bossLevel()){
 			Statistics.floorsExplored.put( depth, level.isLevelExplored(depth));
 		}
 	}
