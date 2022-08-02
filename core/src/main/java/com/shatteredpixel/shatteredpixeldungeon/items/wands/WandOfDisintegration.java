88f2cb7a36c9c096bd0be9c1526142c85a0cdfce
Evan Debenham <Evan@ShatteredPixel.com>
2022-08-02 13:36:11 -0400

    v1.4.0: disintegration no longer harms undiscovered passive chars

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfDisintegration.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfDisintegration.java
@@ -27,6 +27,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Blob;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Web;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
 import com.shatteredpixel.shatteredpixeldungeon.effects.Beam;
 import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
 import com.shatteredpixel.shatteredpixeldungeon.effects.particles.PurpleParticle;
@@ -86,7 +87,12 @@ public class WandOfDisintegration extends DamageWand {
 				terrainBonus += terrainPassed/3;
 				terrainPassed = terrainPassed%3;
 
-				chars.add( ch );
+				if (ch instanceof Mob && ((Mob) ch).state == ((Mob) ch).PASSIVE
+						&& !(Dungeon.level.mapped[c] || Dungeon.level.visited[c])){
+					//avoid harming undiscovered passive chars
+				} else {
+					chars.add(ch);
+				}
 			}
 
 			if (Dungeon.level.solid[c]) {
