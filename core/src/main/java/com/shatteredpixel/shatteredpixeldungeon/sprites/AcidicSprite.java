8d328a62a2192924de974f9d503aeea052e6fc79
Evan Debenham <Evan@ShatteredPixel.com>
2022-09-27 17:29:57 -0400

    v1.4.0: fixed further acidic scorpio sprite issues

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/AcidicSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/AcidicSprite.java
@@ -34,18 +34,18 @@ public class AcidicSprite extends ScorpioSprite {
 		TextureFilm frames = new TextureFilm( texture, 17, 17 );
 		
 		idle = new Animation( 12, true );
-		idle.frames( frames, 14, 14, 14, 14, 14, 14, 14, 14, 15, 16, 15, 16, 15, 16 );
+		idle.frames( frames, 15, 15, 15, 15, 15, 15, 15, 15, 16, 17, 16, 17, 16, 17 );
 		
 		run = new Animation( 4, true );
-		run.frames( frames, 19, 20 );
+		run.frames( frames, 20, 21 );
 		
 		attack = new Animation( 15, false );
-		attack.frames( frames, 14, 17, 18 );
+		attack.frames( frames, 15, 18, 19 );
 		
 		zap = attack.clone();
 		
 		die = new Animation( 12, false );
-		die.frames( frames, 14, 21, 22, 23, 24 );
+		die.frames( frames, 15, 22, 23, 24, 25 );
 		
 		play( idle );
 	}
