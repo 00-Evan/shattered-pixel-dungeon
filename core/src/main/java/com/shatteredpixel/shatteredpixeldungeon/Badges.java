6426649cbabed451b69466276781a7292698487d
Evan Debenham <Evan@ShatteredPixel.com>
2022-10-07 14:35:03 -0400

    v1.4.1: added a safety check to badge restoring

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Badges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Badges.java
@@ -236,6 +236,8 @@ public class Badges {
 		if (bundle == null) return badges;
 		
 		String[] names = bundle.getStringArray( BADGES );
+		if (names == null) return badges;
+
 		for (int i=0; i < names.length; i++) {
 			try {
 				if (renamedBadges.containsKey(names[i])){
