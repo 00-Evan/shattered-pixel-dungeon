4a1940076badbaa3d6d83833929b7548516dd618
Evan Debenham <Evan@ShatteredPixel.com>
2022-10-12 13:18:16 -0400

    v1.4.2: added lullaby vfx to stone of deep sleep

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/stones/StoneOfDeepSleep.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/stones/StoneOfDeepSleep.java
@@ -49,6 +49,7 @@ public class StoneOfDeepSleep extends Runestone {
 			if (c instanceof Mob){
 
 				Buff.affect(c, MagicalSleep.class);
+				c.sprite.centerEmitter().start( Speck.factory( Speck.NOTE ), 0.3f, 5 );
 
 			}
 
