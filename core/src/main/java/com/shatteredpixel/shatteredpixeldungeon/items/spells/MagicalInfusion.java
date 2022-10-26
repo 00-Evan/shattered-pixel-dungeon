9203b225aab1d43b0e8e70b7f11f7b0c0c2cd794
Evan Debenham <Evan@ShatteredPixel.com>
2022-10-26 15:49:19 -0400

    v1.4.2: fixed magical infusion clearing curses on rings and wands

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/spells/MagicalInfusion.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/spells/MagicalInfusion.java
@@ -28,7 +28,9 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Degrade;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.armor.Armor;
+import com.shatteredpixel.shatteredpixeldungeon.items.rings.Ring;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfUpgrade;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
@@ -59,7 +61,11 @@ public class MagicalInfusion extends InventorySpell {
 		} else if (item instanceof Armor && ((Armor) item).glyph != null) {
 			((Armor) item).upgrade(true);
 		} else {
+			boolean wasCursed = item.cursed;
+			boolean wasCurseInfused = item instanceof Wand && ((Wand) item).curseInfusionBonus;
 			item.upgrade();
+			if (wasCursed) item.cursed = true;
+			if (wasCurseInfused) ((Wand) item).curseInfusionBonus = true;
 		}
 		
 		GLog.p( Messages.get(this, "infuse") );
