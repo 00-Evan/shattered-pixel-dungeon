e58eb00e2a56a727201cbc840a8ed095318cc2ed
Evan Debenham <Evan@ShatteredPixel.com>
2022-11-24 16:21:14 -0500

    v2.0.0: fixed duelist unlocked badge being endorsed

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Badges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Badges.java
@@ -867,7 +867,7 @@ public class Badges {
 	}
 
 	public static void validateDuelistUnlock(){
-		if (Dungeon.hero.belongings.weapon instanceof MeleeWeapon
+		if (!isUnlocked(Badge.UNLOCK_HUNTRESS) && Dungeon.hero.belongings.weapon instanceof MeleeWeapon
 				&& ((MeleeWeapon) Dungeon.hero.belongings.weapon).tier >= 2
 				&& ((MeleeWeapon) Dungeon.hero.belongings.weapon).STRReq() <= Dungeon.hero.STR()){
 
