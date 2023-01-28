8e22d013a4495ea8dc428a0f8ea3615b5e03b3a3
Evan Debenham <Evan.SHPX@gmail.com>
2023-01-28 16:27:42 -0500

    v2.0.0: fixed secondary weapon crossbow not visually speeding up darts

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/MissileSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/MissileSprite.java
@@ -151,7 +151,9 @@ public class MissileSprite extends ItemSprite implements Tweener.Listener {
 		}
 		
 		float speed = SPEED;
-		if (item instanceof Dart && Dungeon.hero.belongings.weapon() instanceof Crossbow){
+		if (item instanceof Dart
+				&& (Dungeon.hero.belongings.weapon() instanceof Crossbow
+				|| Dungeon.hero.belongings.secondWep() instanceof Crossbow)){
 			speed *= 3f;
 			
 		} else if (item instanceof SpiritBow.SpiritArrow
