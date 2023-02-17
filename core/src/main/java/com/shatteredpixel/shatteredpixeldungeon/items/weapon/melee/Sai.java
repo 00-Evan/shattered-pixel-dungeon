a8e97a5452761f60febf0f09ef679c90fd61fce8
Evan Debenham <Evan@ShatteredPixel.com>
2023-02-17 13:16:41 -0500

    v2.0.0: fixed combo strike always taking 0 turns

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Sai.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Sai.java
@@ -102,6 +102,7 @@ public class Sai extends MeleeWeapon {
 				}
 
 				Invisibility.dispel();
+				hero.spendAndNext(hero.attackDelay());
 				if (recentHits >= 2 && hit){
 					Sample.INSTANCE.play(Assets.Sounds.HIT_STRONG);
 				}
