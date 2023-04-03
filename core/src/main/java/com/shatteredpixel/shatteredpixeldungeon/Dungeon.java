31afa941b19462d068c4dedf7a1de3b1cd40c32e
Dhanesh Walte <dhaneshwalte@dal.ca>
2023-04-03 10:22:06 -0300

    Added explaining var in observe() to calc dist

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -801,8 +801,8 @@ public class Dungeon {
 	//default to recomputing based on max hero vision, in case vision just shrank/grew
 	public static void observe(){
 		int dist = Math.max(Dungeon.hero.viewDistance, 8);
-		dist *= 1f + 0.25f*Dungeon.hero.pointsInTalent(Talent.FARSIGHT);
-
+		float quarterFarsightPoints = 0.25f*Dungeon.hero.pointsInTalent(Talent.FARSIGHT);
+		dist *= 1f + quarterFarsightPoints;
 		if (Dungeon.hero.buff(MagicalSight.class) != null){
 			dist = Math.max( dist, MagicalSight.DISTANCE );
 		}
