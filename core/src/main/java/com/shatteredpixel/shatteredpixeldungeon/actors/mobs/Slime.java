6ca8f32c829537d481721056edcaecf2abccc1b3
Evan Debenham <Evan@ShatteredPixel.com>
2023-05-24 16:42:35 -0400

    v2.1.0: fixed a rare crash if slimes drop loot when T2 wep deck is empty

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Slime.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Slime.java
@@ -78,8 +78,7 @@ public class Slime extends Mob {
 	public Item createLoot() {
 		Dungeon.LimitedDrops.SLIME_WEP.count++;
 		Generator.Category c = Generator.Category.WEP_T2;
-		MeleeWeapon w = (MeleeWeapon) Reflection.newInstance(c.classes[Random.chances(c.probs)]);
-		w.random();
+		MeleeWeapon w = (MeleeWeapon)Generator.randomUsingDefaults(Generator.Category.WEP_T2);
 		w.level(0);
 		return w;
 	}
