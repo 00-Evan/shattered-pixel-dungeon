42081ae14444c6b5e972658afd74a91280f4c8c1
Evan Debenham <Evan@ShatteredPixel.com>
2023-06-13 16:20:54 -0400

    v2.1.2: becoming assassin while invis now correctly applies prep

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/TengusMask.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/TengusMask.java
@@ -24,6 +24,8 @@ package com.shatteredpixel.shatteredpixeldungeon.items;
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Badges;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Preparation;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
@@ -97,6 +99,10 @@ public class TengusMask extends Item {
 		
 		curUser.subClass = way;
 		Talent.initSubclassTalents(curUser);
+
+		if (way == HeroSubClass.ASSASSIN && curUser.invisible > 0){
+			Buff.affect(curUser, Preparation.class);
+		}
 		
 		curUser.sprite.operate( curUser.pos );
 		Sample.INSTANCE.play( Assets.Sounds.MASTERY );
