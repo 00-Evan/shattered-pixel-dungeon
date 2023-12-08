a11604c72048750639080b25f1ca285d866d7379
Evan Debenham <Evan@ShatteredPixel.com>
2023-12-08 15:42:16 -0500

    v2.3.0: clarified conditions in fixed rot lasher healing text

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/RotLasher.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/RotLasher.java
@@ -55,7 +55,7 @@ public class RotLasher extends Mob {
 
 	@Override
 	protected boolean act() {
-		if (HP < HT && enemy == null || !Dungeon.level.adjacent(pos, enemy.pos)) {
+		if (HP < HT && (enemy == null || !Dungeon.level.adjacent(pos, enemy.pos))) {
 			sprite.showStatusWithIcon(CharSprite.POSITIVE, String.valueOf(Math.min(5, HT - HP)), FloatingText.HEALING);
 			HP = Math.min(HT, HP + 5);
 		}
