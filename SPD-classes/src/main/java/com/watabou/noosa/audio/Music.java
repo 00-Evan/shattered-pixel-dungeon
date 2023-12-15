8695eeb66b0a0b26a023b4cfa978b424e88028ec
Evan Debenham <Evan@ShatteredPixel.com>
2023-12-15 13:11:24 -0500

    v2.3.0: fixed track looping interfering with fade behaviour

--- SPD-classes/src/main/java/com/watabou/noosa/audio/Music.java
+++ SPD-classes/src/main/java/com/watabou/noosa/audio/Music.java
@@ -162,17 +162,20 @@ public enum Music {
 	private com.badlogic.gdx.audio.Music.OnCompletionListener trackLooper = new com.badlogic.gdx.audio.Music.OnCompletionListener() {
 		@Override
 		public void onCompletion(com.badlogic.gdx.audio.Music music) {
-			//we do this in a separate thread to avoid graphics hitching while the music is prepared
-			if (!DeviceCompat.isDesktop()) {
-				new Thread() {
-					@Override
-					public void run() {
-						playNextTrack(music);
-					}
-				}.start();
-			} else {
-				//don't use a separate thread on desktop, causes errors and makes no performance difference
-				playNextTrack(music);
+			//don't play the next track if we're currently in the middle of a fade
+			if (fadeTotal == -1f) {
+				//we do this in a separate thread to avoid graphics hitching while the music is prepared
+				if (!DeviceCompat.isDesktop()) {
+					new Thread() {
+						@Override
+						public void run() {
+							playNextTrack(music);
+						}
+					}.start();
+				} else {
+					//don't use a separate thread on desktop, causes errors and makes no performance difference
+					playNextTrack(music);
+				}
 			}
 		}
 	};
