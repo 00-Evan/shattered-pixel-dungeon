ec165ecbe70d9ea61f539950306a12c0c3d12f74
Evan Debenham <Evan@ShatteredPixel.com>
2024-03-26 16:05:56 -0400

    v2.4.0: fixed armband not updating quickslots when stealing items

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/MasterThievesArmband.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/MasterThievesArmband.java
@@ -302,6 +302,7 @@ public class MasterThievesArmband extends Artifact {
 					GLog.p(Messages.get(MasterThievesArmband.class, "level_up"));
 					upgrade();
 				}
+				updateQuickslot();
 				return true;
 			}
 		}
