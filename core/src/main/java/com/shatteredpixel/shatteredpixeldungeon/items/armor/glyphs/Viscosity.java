c8b4b87e0c7058936cc3232249e99c6f28eb015c
Evan Debenham <Evan@ShatteredPixel.com>
2024-08-08 14:15:27 -0400

    v2.5.0: fixed deferred damage being postponed on save/load

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/armor/glyphs/Viscosity.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/armor/glyphs/Viscosity.java
@@ -123,17 +123,11 @@ public class Viscosity extends Glyph {
 			damage = bundle.getInt( DAMAGE );
 		}
 		
-		@Override
-		public boolean attachTo( Char target ) {
-			if (super.attachTo( target )) {
-				postpone( TICK );
-				return true;
-			} else {
-				return false;
-			}
-		}
-		
 		public void prolong( int damage ) {
+			if (this.damage == 0){
+				//wait 1 turn before damaging if this is freshly applied
+				postpone(TICK);
+			}
 			this.damage += damage;
 		}
 		
