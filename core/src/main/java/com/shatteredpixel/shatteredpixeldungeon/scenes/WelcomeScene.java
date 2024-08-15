503d1b7c19b49f9746967ef2822fcd6a46fdb87d
Evan Debenham <Evan@ShatteredPixel.com>
2024-08-15 14:34:36 -0400

    v2.5.0: updated versions and update message for internal alpha

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/WelcomeScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/WelcomeScene.java
@@ -53,7 +53,7 @@ import java.util.Collections;
 
 public class WelcomeScene extends PixelScene {
 
-	private static final int LATEST_UPDATE = ShatteredPixelDungeon.v2_4_2;
+	private static final int LATEST_UPDATE = 783;
 
 	//used so that the game does not keep showing the window forever if cleaning fails
 	private static boolean triedCleaningTemp = false;
