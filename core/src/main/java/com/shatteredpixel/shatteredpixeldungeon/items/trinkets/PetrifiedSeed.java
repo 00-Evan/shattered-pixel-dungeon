63a9deb300a7b87f7a01bb45dca84f501847ff61
Evan Debenham <Evan@ShatteredPixel.com>
2024-08-28 11:35:54 -0400

    v2.5.0: fixed incorrect desc in petrified seed

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/trinkets/PetrifiedSeed.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/trinkets/PetrifiedSeed.java
@@ -39,12 +39,11 @@ public class PetrifiedSeed extends Trinket {
 	@Override
 	public String statsDesc() {
 		if (isIdentified()){
-
 			return Messages.get(this, "stats_desc",
 					Messages.decimalFormat("#.##", 100*stoneInsteadOfSeedChance(buffedLvl())),
 					Messages.decimalFormat("#.##", 100*(grassLootMultiplier(buffedLvl())-1f)));
 		} else {
-			return Messages.get(this, "stats_desc",
+			return Messages.get(this, "typical_stats_desc",
 					Messages.decimalFormat("#.##", 100*stoneInsteadOfSeedChance(0)),
 					Messages.decimalFormat("#.##", 100*(grassLootMultiplier(0)-1f)));
 		}
