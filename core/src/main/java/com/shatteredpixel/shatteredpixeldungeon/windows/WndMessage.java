34028a2d74b1928b174902b5f36d3702a5017704
Evan Debenham <Evan@ShatteredPixel.com>
2024-09-01 17:01:25 -0400

    v2.5.0: message windows can now dynamically scale their width

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndMessage.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndMessage.java
@@ -27,18 +27,27 @@ import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
 
 public class WndMessage extends Window {
 
-	private static final int WIDTH_P = 120;
-	private static final int WIDTH_L = 144;
+	private static final int WIDTH_MIN = 120;
+	private static final int WIDTH_MAX = 220;
 	private static final int MARGIN = 4;
 	
 	public WndMessage( String text ) {
 		
 		super();
+
+		int width = WIDTH_MIN;
 		
 		RenderedTextBlock info = PixelScene.renderTextBlock( text, 6 );
-		info.maxWidth((PixelScene.landscape() ? WIDTH_L : WIDTH_P) - MARGIN * 2);
+		info.maxWidth(width - MARGIN * 2);
 		info.setPos(MARGIN, MARGIN);
 		add( info );
+
+		while (PixelScene.landscape()
+				&& info.height() > 120
+				&& width < WIDTH_MAX){
+			width += 20;
+			info.maxWidth(width - MARGIN * 2);
+		}
 
 		resize(
 			(int)info.width() + MARGIN * 2,
