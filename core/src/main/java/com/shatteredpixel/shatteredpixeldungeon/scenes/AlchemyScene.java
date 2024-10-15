1ec48f6b26d158ff92d715128bf6e405c9c1a05a
Evan Debenham <Evan@ShatteredPixel.com>
2024-10-15 14:04:03 -0400

    v3.0.0: fixed alchemy guide not being translated

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/AlchemyScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/AlchemyScene.java
@@ -468,7 +468,7 @@ public class AlchemyScene extends PixelScene {
 		sparkEmitter.autoKill = false;
 		add(sparkEmitter);
 
-		StyledButton btnGuide = new StyledButton( Chrome.Type.TOAST_TR, "Guide"){
+		StyledButton btnGuide = new StyledButton( Chrome.Type.TOAST_TR, Messages.get(AlchemyScene.class, "guide")){
 			@Override
 			protected void onClick() {
 				super.onClick();
