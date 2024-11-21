e171a1efee4ec67e5cdcda3fd6b74310b6f71d20
Evan Debenham <Evan@ShatteredPixel.com>
2024-11-21 17:20:29 -0500

    v3.0.0: implemented a changelog for v3.0 so far

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/v3_X_Changes.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/v3_X_Changes.java
@@ -21,18 +21,24 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.ui.changelist;
 
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroClass;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.ChangesScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.HeroSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Icons;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
+import com.watabou.noosa.Image;
 
 import java.util.ArrayList;
 
 public class v3_X_Changes {
 
 	public static void addAllChanges( ArrayList<ChangeInfo> changeInfos ){
-		add_Coming_Soon(changeInfos);
+		//add_Coming_Soon(changeInfos);
 		add_v3_0_Changes(changeInfos);
 	}
 
@@ -57,18 +63,97 @@ public class v3_X_Changes {
 		changes.hardlight(Window.TITLE_COLOR);
 		changeInfos.add(changes);
 
-		changes.addButton( new ChangeButton(Icons.get(Icons.SHPX), "Developer Commentary",
+		/*changes.addButton( new ChangeButton(Icons.get(Icons.SHPX), "Developer Commentary",
 				"_-_ Released ..., ...\n" +
 				"_-_ ... days after Shattered v2.5.0\n\n" +
-				"Dev commentary will be added here in the future."));
+				"Dev commentary will be added here in the future."));*/
+
+		changes.addButton( new ChangeButton(Icons.get(Icons.SHPX), "v3.0.0 To-Do",
+				"Here's a rough list of what needs to be done before v3.0 is ready for release, in addition to changes based on feedback:\n" +
+				"**-** Fully implement the Priest and at least one armor ability\n" +
+				"**-** Release v3.0 to Beta!\n" +
+				"**-** Implement another armor ability\n" +
+				"**-** Implement the Paladin subclass\n" +
+				"**-** Implement a 3rd armor ability\n" +
+				"**-** Polish up the Cleric visually and respond to balance/design feedback\n" +
+				"**-** Do smaller misc. changes for v3.0, primarily some balance and interface tweaks"));
+
+		changes.addButton( new ChangeButton(HeroSprite.avatar(HeroClass.CLERIC, 1), "The Cleric!",
+				"**Shattered Pixel Dungeon has another new hero, making for six total!!**\n" +
+				"\n" +
+				"The Cleric is an entire new hero class **with a variety of unique spells that they learn via talents!** They can be unlocked by removing the curse on any piece of equipment, but existing players who already have a win will automatically unlock them!\n" +
+				"\n" +
+				"Much like how the Duelist can use a bunch of weapon abilities, I want the Cleric to be an ability-centric hero focused on Magic. Unlike the Duelist, these abilities are tied into the hero and their talents, instead of equipment items.\n" +
+				"\n" +
+				"**Note that while the base Cleric is playable, they are not yet fully implemented!** Some visuals are placeholders and not all talents are functional."));
+
+		changes.addButton( new ChangeButton(HeroSprite.avatar(HeroClass.CLERIC, 4), "Cleric Subclasses",
+				"**The Cleric has two subclasses, each with their own emphasis!**\n" +
+				"\n" +
+				"**The Priest** is focused on ranged spell combat, effects that synergize with magical items like wands and artifacts.\n" +
+				"\n" +
+				"**The Paladin has not been implemented yet, look out for them soon!**"));
+
+		changes.addButton( new ChangeButton(HeroSprite.avatar(HeroClass.CLERIC, 5), "Cleric Armor Abilities",
+				"**The Cleric has three lategame armor abilities as well!**\n" +
+				"\n" +
+				"**Currently none of these abilities are implemented. At least one will be before the beta.**"));
 
 		changes = new ChangeInfo(Messages.get(ChangesScene.class, "changes"), false, null);
 		changes.hardlight(CharSprite.WARNING);
 		changeInfos.add(changes);
 
+		changes.addButton( new ChangeButton(Icons.DISPLAY.get(), "Interface Changes",
+				"You can now have up to six runs in progress, and the UI has been adjusted to accommodate for this.\n" +
+				"\n" +
+				"**I also plan to add a bit more information to runs in progress and make some other interface changes before release**"));
+
+		changes.addButton(new ChangeButton(Icons.get(Icons.PREFS), Messages.get(ChangesScene.class, "misc"),
+				"**Highlights:**\n" +
+				"**-** Enemies are now more willing to switch targets if their target is retreating and they are being attacked by something else\n" +
+				"**-** Enemies are now more consistently drawn to the hero's position if they are attacked from out of visible range\n" +
+				"**-** Fixed Reclaim trap spell exploits, and allowed it to be dropped while charged\n" +
+				"**-** Improved visual contrast for symbols on runestones\n" +
+				"\n" +
+				"**Characters:**\n" +
+				"**-** Crazed bandits now have their own description\n" +
+				"\n" +
+				"**Effects:**\n" +
+				"**-** Blast Wave no longer knocks back characters if they are killed over a pit\n" +
+				"\n" +
+				"**Misc.:**\n" +
+				"**-** Camera no longer re-centers on hero when adding custom notes\n" +
+				"**-** Improved text clarity in a few cases\n" +
+				"**-** Updated internal code libraries "));
+
+		changes.addButton(new ChangeButton(new Image(Assets.Sprites.SPINNER, 144, 0, 16, 16), Messages.get(ChangesScene.class, "bugfixes"),
+				"Fixed the following bugs:\n" +
+				"**Highlights:**\n" +
+				"**-** Various cases where closing the game inside of the alchemy scene could result in lost progress\n" +
+				"**-** Runestones working while hero is magic immune\n" +
+				"\n" +
+				"**Items:**\n" +
+				"**-** Cursed armband not updating player gold display on desktop after stealing gold\n" +
+				"**-** Very specific cases where disarming traps could make items unattainable\n" +
+				"**-** Icecap incorrectly being blocked by high grass\n" +
+				"\n" +
+				"**Effects:**\n" +
+				"**-** Monk abilities which ignore armor using the incorrect damage icon\n" +
+				"**-** Damage type not showing when hitting downed crystal guardians\n" +
+				"**-** Very rare cases where spawned high grass could interfere with the floor 20 shop\n" +
+				"**-** Certain effects not having on-death or rankings messages\n" +
+				"**-** Specific cases where cursed wand effects would forget they were spawned by wondrous resin\n" +
+				"\n" +
+				"**Misc.:**\n" +
+				"**-** Various minor textual errors\n" +
+				"**-** Various rare crash errors"));
+
 		changes = new ChangeInfo(Messages.get(ChangesScene.class, "buffs"), false, null);
 		changes.hardlight(CharSprite.POSITIVE);
 		changeInfos.add(changes);
+
+		changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.SOMETHING), "Nothing yet!",
+				"While there are no other balance changes in v3.0 currently, I do plan to go over some balance numbers and make adjustments here before release."));
 
 		changes = new ChangeInfo(Messages.get(ChangesScene.class, "nerfs"), false, null);
 		changes.hardlight(CharSprite.NEGATIVE);
