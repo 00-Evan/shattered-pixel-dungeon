ec95fcc5dd3da41f07ee2bb7af9bb3679b416745
Evan Debenham <Evan@ShatteredPixel.com>
2024-11-27 10:47:34 -0500

    v3.0.0: fixed stone of detect magic being usable on thrown items

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/stones/StoneOfDetectMagic.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/stones/StoneOfDetectMagic.java
@@ -28,6 +28,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.armor.Armor;
 import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles.MissileWeapon;
 import com.shatteredpixel.shatteredpixeldungeon.journal.Catalog;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
@@ -43,6 +44,7 @@ public class StoneOfDetectMagic extends InventoryStone {
 	@Override
 	public boolean usableOnItem(Item item){
 		return (item instanceof EquipableItem || item instanceof Wand)
+				&& !(item instanceof MissileWeapon)
 				&& (!item.isIdentified() || !item.cursedKnown);
 	}
 
