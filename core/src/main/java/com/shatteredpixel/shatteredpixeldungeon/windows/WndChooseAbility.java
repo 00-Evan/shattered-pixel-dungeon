b8f7a4bdca7b3f23ce8fabb942f1c18365bcb759
Evan Debenham <Evan@ShatteredPixel.com>
2025-01-23 13:50:31 -0500

    v3.0.0: added a warning when people select Trinity with older data

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndChooseAbility.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndChooseAbility.java
@@ -68,12 +68,18 @@ public class WndChooseAbility extends Window {
 		float pos = body.bottom() + 3*GAP;
 		for (ArmorAbility ability : hero.heroClass.armorAbilities()) {
 
+			String warn;
+			if (Dungeon.initialVersion < 820){
+				warn = "_WARNING, code to track which items you have found for use in trinity was added in BETA-2.1. This run was started before that, and so some items you have encountered may not be usable with Trinity. Any items you currently hold can be made selectable by dropping and picking them back up._\n\n";
+			} else {
+				warn = "";
+			}
 			RedButton abilityButton = new RedButton(ability.shortDesc(), 6){
 				@Override
 				protected void onClick() {
 					GameScene.show(new WndOptions( new HeroIcon( ability ),
 							Messages.titleCase(ability.name()),
-							Messages.get(WndChooseAbility.this, "are_you_sure"),
+							warn + Messages.get(WndChooseAbility.this, "are_you_sure"),
 							Messages.get(WndChooseAbility.this, "yes"),
 							Messages.get(WndChooseAbility.this, "no")){
 
