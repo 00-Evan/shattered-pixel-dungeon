9f3a31a21a379576b869429771f6b46c00476fa9
Evan Debenham <Evan@ShatteredPixel.com>
2025-02-18 15:58:51 -0500

    v3.0.0: fixed rare quickslot errors after upgrading durable projectiles

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Talent.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Talent.java
@@ -526,7 +526,8 @@ public enum Talent {
 			Dungeon.observe();
 		}
 
-		if (talent == TWIN_UPGRADES || talent == DESPERATE_POWER || talent == STRONGMAN){
+		if (talent == TWIN_UPGRADES || talent == DESPERATE_POWER
+				|| talent == STRONGMAN || talent == DURABLE_PROJECTILES){
 			Item.updateQuickslot();
 		}
 
