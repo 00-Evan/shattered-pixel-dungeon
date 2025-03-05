18479aae92c3586c628263e7259c145c1df0aa9c
Evan Debenham <Evan@ShatteredPixel.com>
2025-03-05 17:23:55 -0500

    v3.0.1: alchemy guide icons are no longer greyed out in main menu

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/QuickRecipe.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/QuickRecipe.java
@@ -124,12 +124,14 @@ public class QuickRecipe extends Component {
 					if (sim.getClass() != in.getClass() || sim.isIdentified())
 						quantity += sim.quantity();
 				}
-			}
-			
-			if (quantity < in.quantity()) {
-				curr.sprite.alpha(0.3f);
+				if (quantity < in.quantity()) {
+					curr.sprite.alpha(0.3f);
+					hasInputs = false;
+				}
+			} else {
 				hasInputs = false;
 			}
+
 			curr.showExtraInfo(false);
 			add(curr);
 			this.inputs.add(curr);
@@ -159,7 +161,7 @@ public class QuickRecipe extends Component {
 				ShatteredPixelDungeon.scene().addToFront(new WndInfoItem(output));
 			}
 		};
-		if (!hasInputs){
+		if (Dungeon.hero != null && !hasInputs){
 			this.output.sprite.alpha(0.3f);
 		}
 		this.output.showExtraInfo(false);
