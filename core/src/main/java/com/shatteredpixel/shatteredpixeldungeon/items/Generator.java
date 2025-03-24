2d42d9307c53f2ae760aea294c0e3473114d82af
lordlen <lennardchan@ymail.com>
2025-03-24 19:29:20 -0400

    nerf flow and remove haste

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
@@ -76,7 +76,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfEnergy;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfEvasion;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfForce;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfFuror;
-import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfHaste;
+// import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfHaste;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfMight;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfSharpshooting;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.RingOfTenacity;
@@ -546,7 +546,7 @@ public class Generator {
 					RingOfEvasion.class,
 					RingOfForce.class,
 					RingOfFuror.class,
-					RingOfHaste.class,
+					// RingOfHaste.class,
 					RingOfMight.class,
 					RingOfSharpshooting.class,
 					RingOfTenacity.class,
