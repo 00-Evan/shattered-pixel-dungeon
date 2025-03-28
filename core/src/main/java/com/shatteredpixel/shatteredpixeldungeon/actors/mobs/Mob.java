564b74129364a33da4d909af04332ed7841d5d22
Evan Debenham <Evan@ShatteredPixel.com>
2025-03-28 13:26:47 -0400

    v3.1.0: Improved Tengu's target selection, no longer always atks hero

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Mob.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Mob.java
@@ -1209,7 +1209,7 @@ public abstract class Mob extends Char {
 		public static final String TAG	= "HUNTING";
 
 		//prevents rare infinite loop cases
-		private boolean recursing = false;
+		protected boolean recursing = false;
 
 		@Override
 		public boolean act( boolean enemyInFOV, boolean justAlerted ) {
