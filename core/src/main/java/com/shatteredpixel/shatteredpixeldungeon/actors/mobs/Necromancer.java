0743ac3959158fe81c7b2a6aa11df7782078541b
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-03-30 17:57:17 -0300

    Necro move fix + Sentry cancel mechanic back

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Necromancer.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Necromancer.java
@@ -246,6 +246,13 @@ public class Necromancer extends Mob {
 	public void summonMinion(){
 		if (Actor.findChar(summoningPos) != null) {
 
+			//cancel if character cannot be moved
+			if (Char.hasProp(Actor.findChar(summoningPos), Property.IMMOVABLE)){
+				summoning = false;
+				((NecromancerSprite)sprite).finishSummoning();
+				return;
+			}
+
 			int pushPos = pos;
 			for (int c : PathFinder.NEIGHBOURS8) {
 				if (Actor.findChar(summoningPos + c) == null
@@ -427,7 +434,7 @@ public class Necromancer extends Mob {
 				}
 
 			} else {
-				if(!enemySeen) return super.act(enemyInFOV, justAlerted);
+				if(!enemySeen || Dungeon.level.distance(pos, enemy.pos) > 4) return super.act(enemyInFOV, justAlerted);
 				else {
 					spend(TICK);
 					return true;
