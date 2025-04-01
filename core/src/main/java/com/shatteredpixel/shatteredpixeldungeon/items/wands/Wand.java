e0752b12ca68b0f1d4a90a5dcf4c3fb855dbbd58
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-04-01 08:07:42 -0300

    Reverted Wand changes

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/Wand.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/Wand.java
@@ -542,12 +542,11 @@ public abstract class Wand extends Item {
 	
 	@Override
 	public Item random() {
-		//POLISHED: adjusted chances to match melee weapons
-		//+0: 75% (3/4)
-		//+1: 20% (4/20)
-		//+2: 5%  (1/20)
+		//+0: 66.67% (2/3)
+		//+1: 26.67% (4/15)
+		//+2: 6.67%  (1/15)
 		int n = 0;
-		if (Random.Int(4) == 0) {
+		if (Random.Int(3) == 0) {
 			n++;
 			if (Random.Int(5) == 0) {
 				n++;
