73cd99c309978260713861dd99d8ba3669a31eee
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-04-02 07:42:21 -0300

    Warlock adjustments
    No longer heals while starving,
    reduced heal ratio
    
    *Getting into starvation no longer hurts you the turn it starts, so soulmark doesnt constantly take chip damage from going in and out

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Mob.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Mob.java
@@ -750,13 +750,14 @@ public abstract class Mob extends Char {
 				restoration = Math.round(restoration * 0.4f*Dungeon.hero.pointsInTalent(Talent.SOUL_SIPHON)/3f);
 			}
 			if (restoration > 0) {
-				Buff.affect(Dungeon.hero, Hunger.class).affectHunger(restoration*Dungeon.hero.pointsInTalent(Talent.SOUL_EATER)/3f);
-
-				if (Dungeon.hero.HP < Dungeon.hero.HT) {
-					int heal = (int)Math.ceil(restoration * 0.4f);
+				if (Dungeon.hero.HP < Dungeon.hero.HT && !Dungeon.hero.isStarving()) {
+					int heal = (int)Math.ceil(restoration * SoulMark.Polished.healRatio);
 					Dungeon.hero.HP = Math.min(Dungeon.hero.HT, Dungeon.hero.HP + heal);
 					Dungeon.hero.sprite.showStatusWithIcon(CharSprite.POSITIVE, Integer.toString(heal), FloatingText.HEALING);
 				}
+
+				//we specifically restore hunger after the heal check so starving characters don't heal
+				Buff.affect(Dungeon.hero, Hunger.class).affectHunger(restoration*Dungeon.hero.pointsInTalent(Talent.SOUL_EATER)/3f);
 			}
 		}
 
