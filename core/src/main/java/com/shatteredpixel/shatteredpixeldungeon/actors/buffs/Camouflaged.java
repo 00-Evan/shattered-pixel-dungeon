cd686b245ccc7a5c35c8e473141a4dffce8ffddf
lordlen <lennardchan@ymail.com>
2025-04-09 20:59:19 -0400

    camouflage glyph now uses camouflage buff

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Camouflaged.java
@@ -0,0 +1,77 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.actors.buffs;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
+import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.Bundle;
+
+public class Camouflaged extends FlavourBuff {
+	
+	@Override
+	public int icon() {
+		return BuffIndicator.SHADOWS;
+	}
+
+	@Override
+	public String desc() {
+		return Messages.get(this, "desc");
+	}
+
+	@Override
+	public boolean attachTo( Char target ) {
+		if (super.attachTo( target )) {
+			target.camouflaged++;
+			if (target instanceof Hero && ((Hero) target).subClass == HeroSubClass.ASSASSIN){
+				Buff.affect(target, Preparation.class);
+			}
+			if (target instanceof Hero && ((Hero) target).hasTalent(Talent.PROTECTIVE_SHADOWS)){
+				Buff.affect(target, Talent.ProtectiveShadowsTracker.class);
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void detach() {
+		if (target.camouflaged > 0)
+			target.camouflaged--;
+		super.detach();
+	}
+
+	@Override
+	public void fx(boolean on) {
+		if (on) target.sprite.add( CharSprite.State.INVISIBLE );
+		else if (!target.isStealthy()) target.sprite.remove( CharSprite.State.INVISIBLE );
+	}
+}
