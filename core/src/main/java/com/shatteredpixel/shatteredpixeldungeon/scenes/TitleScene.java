456c2e11a9b5ef8d31d7a923f0155cc42adb0b0a
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-04-29 20:20:11 -0300

    Adjusted title fireball positions

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/TitleScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/TitleScene.java
@@ -87,11 +87,11 @@ public class TitleScene extends PixelScene {
 		align(title);
 
 		if (landscape()){
-			placeTorch(title.x + 30, title.y + 35);
-			placeTorch(title.x + title.width - 30, title.y + 35);
+			placeTorch(title.x + 30, title.y + 44);
+			placeTorch(title.x + title.width - 30, title.y + 44);
 		} else {
-			placeTorch(title.x + 16, title.y + 70);
-			placeTorch(title.x + title.width - 16, title.y + 70);
+			placeTorch(title.x + 16, title.y + 64);
+			placeTorch(title.x + title.width - 16, title.y + 64);
 		}
 
 		Image signs = new Image(BannerSprites.get( landscape() ? BannerSprites.Type.TITLE_GLOW_LAND : BannerSprites.Type.TITLE_GLOW_PORT)){
