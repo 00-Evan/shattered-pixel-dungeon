0724c52a2ca1a921ef206f64d8cec944dfd81dae
BacalaoPlusPlus <federico.simkus@gmail.com>
2025-05-05 01:01:50 -0300

    Gnoll Brute buffs

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Brute.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Brute.java
@@ -26,15 +26,21 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.AscensionChallenge;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.ShieldBuff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.ArmorAbility;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.spells.ClericSpell;
 import com.shatteredpixel.shatteredpixeldungeon.effects.FloatingText;
+import com.shatteredpixel.shatteredpixeldungeon.effects.MagicMissile;
 import com.shatteredpixel.shatteredpixeldungeon.effects.SpellSprite;
 import com.shatteredpixel.shatteredpixeldungeon.items.Gold;
+import com.shatteredpixel.shatteredpixeldungeon.items.armor.glyphs.Viscosity;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
 import com.shatteredpixel.shatteredpixeldungeon.levels.features.Chasm;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.BruteSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
 import com.watabou.utils.Bundle;
+import com.watabou.utils.GameMath;
 import com.watabou.utils.Random;
 
 public class Brute extends Mob {
@@ -69,6 +75,40 @@ public class Brute extends Mob {
 	@Override
 	public int drRoll() {
 		return super.drRoll() + Random.NormalIntRange(0, 8);
+	}
+
+	@Override
+	public void damage( int dmg, Object src ) {
+		if (!isAlive() || dmg < 0) {
+			return;
+		}
+		if (isInvulnerable(src.getClass())) {
+			sprite.showStatus(CharSprite.POSITIVE, Messages.get(this, "invulnerable"));
+			return;
+		}
+		if (src instanceof Viscosity.DeferedDamage) super.damage(dmg, src);
+
+
+		Char attacker = null;
+		if(src instanceof Char) attacker = (Char)src;
+		//assume the hero is hitting us in these common cases
+		else if (src instanceof Wand || src instanceof ClericSpell || src instanceof ArmorAbility) attacker = Dungeon.hero;
+		else if (enemy != null) attacker = enemy;
+
+		int separation = attacker != null ? Dungeon.level.distance(pos, attacker.pos)-1 : 10;
+		float percent = GameMath.gate(0f, 0.15f * separation, 1f);
+
+		int deferred = Math.round(percent * dmg);
+		dmg -= deferred;
+
+		if (deferred > 0){
+			Viscosity.DeferedDamage def = Buff.affect( this, Viscosity.DeferedDamage.class );
+			def.extend( deferred );
+
+			sprite.showStatus( CharSprite.WARNING, Messages.get(Viscosity.class, "deferred", deferred) );
+		}
+
+		super.damage(dmg, src);
 	}
 
 	@Override
@@ -130,7 +170,7 @@ public class Brute extends Mob {
 				return true;
 			}
 			
-			absorbDamage( Math.round(4*AscensionChallenge.statModifier(target)));
+			absorbDamage( Math.round(3*AscensionChallenge.statModifier(target)));
 			
 			if (shielding() <= 0){
 				target.die(null);
