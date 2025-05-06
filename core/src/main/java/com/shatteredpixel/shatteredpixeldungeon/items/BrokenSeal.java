d15f1073a2903fa136794946f8a74b23d516a4c8
Evan Debenham <Evan@ShatteredPixel.com>
2025-05-06 11:44:55 -0400

    v3.1.0: Warrior's broken seal no longer recharges when regen is off

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/BrokenSeal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/BrokenSeal.java
@@ -23,6 +23,7 @@ package com.shatteredpixel.shatteredpixeldungeon.items;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Regeneration;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.ShieldBuff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Belongings;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
@@ -318,7 +319,7 @@ public class BrokenSeal extends Item {
 
 		@Override
 		public synchronized boolean act() {
-			if (cooldown > 0){
+			if (cooldown > 0 && Regeneration.regenOn()){
 				cooldown--;
 			}
 
