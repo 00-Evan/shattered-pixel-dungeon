f7dbb4c0a7bcf7bc50c679326e04004fe1f9763b
dmacthedestroyer <mcdonald.daniel.james@gmail.com>
2025-06-08 09:33:53 -0700

    Simple (and largely untested) modifications to allow programmable AI agents

---
+++ ai/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ai/AILauncher.java
@@ -0,0 +1,198 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.ai;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3FileHandle;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Preferences;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowListener;
+import com.badlogic.gdx.utils.SharedLibraryLoader;
+import com.shatteredpixel.shatteredpixeldungeon.SPDSettings;
+import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
+import com.shatteredpixel.shatteredpixeldungeon.services.news.News;
+import com.shatteredpixel.shatteredpixeldungeon.services.news.NewsImpl;
+import com.shatteredpixel.shatteredpixeldungeon.services.updates.UpdateImpl;
+import com.shatteredpixel.shatteredpixeldungeon.services.updates.Updates;
+import com.shatteredpixel.shatteredpixeldungeon.desktop.DesktopLaunchValidator;
+import com.shatteredpixel.shatteredpixeldungeon.desktop.DesktopWindowListener;
+import com.shatteredpixel.shatteredpixeldungeon.desktop.DesktopPlatformSupport;
+import com.watabou.noosa.Game;
+import com.watabou.utils.FileUtils;
+import com.watabou.utils.Point;
+
+import org.lwjgl.util.tinyfd.TinyFileDialogs;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Locale;
+
+/**
+ * This class is mostly copied from the other DesktopLauncher class, maybe more modifications will be needed down the road
+ */
+public class AILauncher {
+
+	public static void main (String[] args) {
+
+		if (!DesktopLaunchValidator.verifyValidJVMState(args)){
+			return;
+		}
+
+		//detection for FreeBSD (which is equivalent to linux for us)
+		//TODO might want to merge request this to libGDX
+		if (System.getProperty("os.name").contains("FreeBSD")) {
+			SharedLibraryLoader.isLinux = true;
+			//this overrides incorrect values set in SharedLibraryLoader's static initializer
+			SharedLibraryLoader.isIos = false;
+			SharedLibraryLoader.is64Bit = System.getProperty("os.arch").contains("64") || System.getProperty("os.arch").startsWith("armv8");
+		}
+		
+		final String title;
+		if (AILauncher.class.getPackage().getSpecificationTitle() == null){
+			title = System.getProperty("Specification-Title");
+		} else {
+			title = AILauncher.class.getPackage().getSpecificationTitle();
+		}
+		
+		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			@Override
+			public void uncaughtException(Thread thread, Throwable throwable) {
+				Game.reportException(throwable);
+				StringWriter sw = new StringWriter();
+				PrintWriter pw = new PrintWriter(sw);
+				throwable.printStackTrace(pw);
+				pw.flush();
+				String exceptionMsg = sw.toString();
+
+				//shorten/simplify exception message to make it easier to fit into a message box
+				exceptionMsg = exceptionMsg.replaceAll("\\(.*:([0-9]*)\\)", "($1)");
+				exceptionMsg = exceptionMsg.replace("com.shatteredpixel.shatteredpixeldungeon.", "");
+				exceptionMsg = exceptionMsg.replace("com.watabou.", "");
+				exceptionMsg = exceptionMsg.replace("com.badlogic.gdx.", "");
+				exceptionMsg = exceptionMsg.replace("\t", "  "); //shortens length of tabs
+
+				//replace ' and " with similar equivalents as tinyfd hates them for some reason
+				exceptionMsg = exceptionMsg.replace('\'', '’');
+				exceptionMsg = exceptionMsg.replace('"', '”');
+
+				if (exceptionMsg.length() > 1000){
+					exceptionMsg = exceptionMsg.substring(0, 1000) + "...";
+				}
+
+				if (exceptionMsg.contains("Couldn’t create window")){
+					TinyFileDialogs.tinyfd_messageBox(title + " Has Crashed!",
+							title + " was not able to initialize its graphics display, sorry about that!\n\n" +
+									"This usually happens when your graphics card has misconfigured drivers or does not support openGL 2.0+.\n\n" +
+									"If you are certain the game should work on your computer, please message the developer (Evan@ShatteredPixel.com)\n\n" +
+									"version: " + Game.version + "\n" +
+									exceptionMsg,
+							"ok", "error", false);
+				} else {
+					TinyFileDialogs.tinyfd_messageBox(title + " Has Crashed!",
+							title + " has run into an error it cannot recover from and has crashed, sorry about that!\n\n" +
+									"If you could, please email this error message to the developer (Evan@ShatteredPixel.com):\n\n" +
+									"version: " + Game.version + "\n" +
+									exceptionMsg,
+							"ok", "error", false);
+				}
+				System.exit(1);
+			}
+		});
+		
+		Game.version = AILauncher.class.getPackage().getSpecificationVersion();
+		if (Game.version == null) {
+			Game.version = System.getProperty("Specification-Version");
+		}
+		
+		try {
+			Game.versionCode = Integer.parseInt(AILauncher.class.getPackage().getImplementationVersion());
+		} catch (NumberFormatException e) {
+			Game.versionCode = Integer.parseInt(System.getProperty("Implementation-Version"));
+		}
+
+		if (UpdateImpl.supportsUpdates()){
+			Updates.service = UpdateImpl.getUpdateService();
+		}
+		if (NewsImpl.supportsNews()){
+			News.service = NewsImpl.getNewsService();
+		}
+		
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		
+		config.setTitle( title );
+
+		//if I were implementing this from scratch I would use the full implementation title for saves
+		// (e.g. /.shatteredpixel/shatteredpixeldungeon), but we have too much existing save
+		// date to worry about transferring at this point.
+		String vendor = AILauncher.class.getPackage().getImplementationTitle();
+		if (vendor == null) {
+			vendor = System.getProperty("Implementation-Title");
+		}
+		vendor = vendor.split("\\.")[1];
+
+		String basePath = "";
+		Files.FileType baseFileType = null;
+		if (SharedLibraryLoader.isWindows) {
+			if (System.getProperties().getProperty("os.name").equals("Windows XP")) {
+				basePath = "Application Data/." + vendor + "/" + title + "/";
+			} else {
+				basePath = "AppData/Roaming/." + vendor + "/" + title + "/";
+			}
+			baseFileType = Files.FileType.External;
+		} else if (SharedLibraryLoader.isMac) {
+			basePath = "Library/Application Support/" + title + "/";
+			baseFileType = Files.FileType.External;
+		} else if (SharedLibraryLoader.isLinux) {
+			String XDGHome = System.getenv("XDG_DATA_HOME");
+			if (XDGHome == null) XDGHome = System.getProperty("user.home") + "/.local/share";
+
+			String titleLinux = title.toLowerCase(Locale.ROOT).replace(" ", "-");
+			basePath = XDGHome + "/." + vendor + "/" + titleLinux + "/";
+
+			baseFileType = Files.FileType.Absolute;
+		}
+
+		config.setPreferencesConfig( basePath, baseFileType );
+		SPDSettings.set( new Lwjgl3Preferences( new Lwjgl3FileHandle(basePath + SPDSettings.DEFAULT_PREFS_FILE, baseFileType) ));
+		FileUtils.setDefaultFileProperties( baseFileType, basePath );
+		
+		config.setWindowSizeLimits( 720, 400, -1, -1 );
+		Point p = SPDSettings.windowResolution();
+		config.setWindowedMode( p.x, p.y );
+
+		config.setMaximized(SPDSettings.windowMaximized());
+
+		//going fullscreen on launch is a bit buggy
+		// so game always starts windowed and then switches in DesktopPlatformSupport.updateSystemUI
+		//config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+
+		//records whether window is maximized or not for settings
+		Lwjgl3WindowListener listener = new DesktopWindowListener();
+		config.setWindowListener( listener );
+		
+		config.setWindowIcon("icons/icon_16.png", "icons/icon_32.png", "icons/icon_48.png",
+				"icons/icon_64.png", "icons/icon_128.png", "icons/icon_256.png");
+
+		new Lwjgl3Application(new AIPixelDungeon(new DesktopPlatformSupport()), config);
+	}
+}
---
+++ ai/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ai/AIPixelDungeon.java
@@ -0,0 +1,71 @@
+package com.shatteredpixel.shatteredpixeldungeon.ai;
+
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroAction;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.watabou.utils.PathFinder;
+import com.watabou.utils.PlatformSupport;
+
+/**
+ * Inject programmable hero actions into the standard ShatteredPixelDungeon Game class. Future updates will make the
+ * Hero agent parameterized so we can plug in different agents.
+ */
+public class AIPixelDungeon extends ShatteredPixelDungeon {
+    public AIPixelDungeon(PlatformSupport platform) {
+        super(platform);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (Dungeon.hero != null && Dungeon.hero.isAlive() && Dungeon.hero.ready && scene.active && scene.alive && scene.getClass().equals(GameScene.class) && Dungeon.hero.curAction == null && Dungeon.level != null) {
+            HeroAction action = act();
+            if (action != null) {
+                Dungeon.hero.curAction = action;
+                // stole this from CellSelector.moveFromActions function
+                // I don't understand yet how calling next() progresses the game logic, but it seems to do the trick ¯\_(ツ)_/¯
+                Dungeon.hero.next();
+            }
+        }
+
+    }
+
+    /**
+     * Modify this code to make the AI hero do whatever you want. If it returns any subclass of HeroAction, it'll
+     * perform that action. Otherwise does nothing. I don't know how robust this is, so give it a try and report any
+     * errors found
+     */
+    protected HeroAction act() {
+        // move towards the closest unexplored accessible cell
+        int closestCell = 999999999;
+        // iterate through the visited array to find cells we haven't seen yet
+        // (I don't really know what "visited" means -- just that it's been in our POV before? We've walked on it?
+        for (int target = 0; target < Dungeon.level.visited.length && closestCell == 999999999; target++) {
+            if (!Dungeon.level.visited[target]) {
+                int distance = Dungeon.level.distance(Dungeon.hero.pos, target);
+                if (distance < closestCell) {
+                    // I stole this from the Hero.getCloser function
+                    int len = Dungeon.level.length();
+                    boolean[] p = Dungeon.level.passable;
+                    boolean[] v = Dungeon.level.visited;
+                    boolean[] m = Dungeon.level.mapped;
+                    boolean[] passable = new boolean[len];
+                    for (int i = 0; i < len; i++) {
+                        passable[i] = p[i] && (v[i] || m[i]);
+                    }
+
+                    // make sure the target cell is accessible (also stolen from Hero class)
+                    PathFinder.Path newpath = Dungeon.findPath(Dungeon.hero, target, passable, Dungeon.hero.fieldOfView, true);
+                    if (newpath != null) {
+                        closestCell = target;
+                    }
+                }
+            }
+        }
+        if (closestCell != 999999999) {
+            return new HeroAction.Move(closestCell);
+        }
+        return null;
+     }
+}
