12c2e163ef6507c4b60bb924197ac06c91ae4d1b
Evan Debenham <Evan@ShatteredPixel.com>
2025-07-23 14:35:57 -0400

    v3.2.0: gladiator buffs: - combo now always lasts 15 turns on kill - cleave buffed to 30/45/60 turns from 15/30/45

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
@@ -93,7 +93,7 @@ public class Combo extends Buff implements ActionIndicator.Action {
 		comboTime = 5f;
 
 		if (!enemy.isAlive() || (enemy.buff(Corruption.class) != null && enemy.HP == enemy.HT)){
-			comboTime = Math.max(comboTime, 15*((Hero)target).pointsInTalent(Talent.CLEAVE));
+			comboTime = 15f + 15f*((Hero)target).pointsInTalent(Talent.CLEAVE);
 		}
 
 		initialComboTime = comboTime;
