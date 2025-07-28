dd328bc2594b15adafd3a7a9d6863c3f9648c917
Evan Debenham <Evan@ShatteredPixel.com>
2025-07-28 12:55:30 -0400

    v3.2.0: transmuting a thrown set no longer repairs it

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/ScrollOfTransmutation.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/ScrollOfTransmutation.java
@@ -262,6 +262,8 @@ public class ScrollOfTransmutation extends InventoryScroll {
 		//technically a new set, ensure old one is destroyed (except for darts)
 		if (w instanceof MissileWeapon && w.isUpgradable()){
 			Buff.affect(Dungeon.hero, MissileWeapon.UpgradedSetTracker.class).levelThresholds.put(((MissileWeapon) w).setID, Integer.MAX_VALUE);
+			//also extra missile weapon properties
+			((MissileWeapon) n).damage(100 - ((MissileWeapon)w).durabilityLeft());
 		}
 		
 		return n;
