42a8c442ee4635eab6f1831b23bcce5be276ac2c
Evan Debenham <Evan@ShatteredPixel.com>
2025-08-25 13:28:51 -0400

    v3.2.2: game now no longer toggles immersive if there is no bar to hide

--- android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidPlatformSupport.java
+++ android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidPlatformSupport.java
@@ -26,9 +26,11 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Build;
 import android.view.View;
+import android.view.WindowInsets;
 import android.view.WindowManager;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.PixmapPacker;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
@@ -73,7 +75,7 @@ public class AndroidPlatformSupport extends PlatformSupport {
 							WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 				}
 
-				if (SPDSettings.fullscreen()) {
+				if (supportsFullScreen() && SPDSettings.fullscreen()) {
 					AndroidLauncher.instance.getWindow().getDecorView().setSystemUiVisibility(
 							View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 									| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN
