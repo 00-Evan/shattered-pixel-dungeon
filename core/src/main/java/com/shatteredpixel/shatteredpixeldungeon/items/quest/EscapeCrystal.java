308287de64a78102f933c4a43f0ae804360195ab
Evan Debenham <Evan@ShatteredPixel.com>
2025-12-03 11:04:16 -0500

    v3.3.0: fixed possible NPE in escape crystal

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
@@ -95,7 +95,7 @@ public class EscapeCrystal extends Item {
 						LevelTransition.Type.BRANCH_EXIT);
 				InterlevelScene.mode = InterlevelScene.Mode.ASCEND;
 				Game.switchScene( InterlevelScene.class );
-				detach(hero.belongings.backpack);
+				detachAll(hero.belongings.backpack);
 
 			}
 
