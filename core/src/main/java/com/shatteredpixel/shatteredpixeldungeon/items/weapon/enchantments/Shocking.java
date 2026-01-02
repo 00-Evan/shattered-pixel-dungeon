218f668c8ba7d2f5d8fe590d549ecb3d366a0190
Evan Debenham <Evan@ShatteredPixel.com>
2026-01-02 11:37:34 -0500

    v3.3.2: shocking enchant no longer scales with both dmg and proc chance
    proc chance now a flat 1/3, down from 1+lvl/4+lvl
    dmg up to 50% from 40%

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/enchantments/Shocking.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/enchantments/Shocking.java
@@ -44,10 +44,8 @@ public class Shocking extends Weapon.Enchantment {
 	public int proc( Weapon weapon, Char attacker, Char defender, int damage ) {
 		int level = Math.max( 0, weapon.buffedLvl() );
 
-		// lvl 0 - 25%
-		// lvl 1 - 40%
-		// lvl 2 - 50%
-		float procChance = (level+1f)/(level+4f) * procChanceMultiplier(attacker);
+		// flat 33% proc chance, effect scales with level via damage dealt
+		float procChance = (1/3f) * procChanceMultiplier(attacker);
 		if (Random.Float() < procChance) {
 
 			float powerMulti = Math.max(1f, procChance);
@@ -60,7 +58,7 @@ public class Shocking extends Weapon.Enchantment {
 			affected.remove(defender); //defender isn't hurt by lightning
 			for (Char ch : affected) {
 				if (ch.alignment != attacker.alignment) {
-					ch.damage(Math.round(damage * 0.4f * powerMulti), this);
+					ch.damage(Math.round(damage * 0.5f * powerMulti), this);
 				}
 			}
 
