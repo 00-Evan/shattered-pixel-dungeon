5561d2da9049ac86d722a4b84c34d573565c557f
Evan Debenham <Evan@ShatteredPixel.com>
2026-06-24 13:50:07 -0400

    v4.0.0: commited vault token door sprite (should've been 2 ago, oops)

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/VaultTokenDoorSprite.java
@@ -0,0 +1,59 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2026 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.sprites;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.watabou.noosa.TextureFilm;
+
+public class VaultTokenDoorSprite extends MobSprite {
+
+	public VaultTokenDoorSprite () {
+		super();
+
+		perspectiveRaise = 4/16f; //2 pixels less, to align with door visuals
+		renderShadow = false;
+		visibleOutOfFFOV = true;
+
+		texture(Assets.Sprites.VAULT_TOKENS_DOOR );
+
+		TextureFilm frames = new TextureFilm( texture, 16, 16 );
+
+		idle = new Animation( 1, false );
+		idle.frames( frames, 0 );
+
+		run = idle.clone();
+
+		attack = idle.clone();
+
+		die = idle.clone();
+
+		play( idle );
+	}
+
+	@Override
+	public void link(Char ch) {
+		super.link(ch);
+		renderShadow = false;
+	}
+
+}
