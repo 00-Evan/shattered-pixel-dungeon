ae756759955b4e43bb025b7f3aed7142a3d824cf
Evan Debenham <Evan@ShatteredPixel.com>
2026-07-02 14:32:32 -0400

    v4.0.0: added carpets for sewers and prison (mostly unused for now)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/tiles/custom/Carpet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/tiles/custom/Carpet.java
@@ -74,7 +74,7 @@ public class Carpet extends CustomTilemap {
 	public Tilemap create() {
 		Tilemap v = super.create();
 		int[] data = new int[tileW*tileH];
-		int regionOfs = 16 * (int)(Dungeon.depth/5);
+		int regionOfs = 16 * (int)((Dungeon.depth-1)/5);
 		int i = 0;
 		for (int y = 0; y < tileH; y++){
 			for (int x = 0; x < tileW; x++){
