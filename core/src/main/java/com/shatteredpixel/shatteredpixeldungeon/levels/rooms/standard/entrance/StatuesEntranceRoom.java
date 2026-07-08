da7586db421bfb963bdbd70be07dda042270cbde
Evan Debenham <Evan@ShatteredPixel.com>
2026-07-08 19:46:26 -0400

    v4.0.0: improved new visuals in large statues entrance and exit rooms

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/entrance/StatuesEntranceRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/entrance/StatuesEntranceRoom.java
@@ -26,8 +26,10 @@ import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.shatteredpixel.shatteredpixeldungeon.levels.features.LevelTransition;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.StatuesRoom;
+import com.shatteredpixel.shatteredpixeldungeon.tiles.custom.Carpet;
 import com.watabou.utils.PathFinder;
 import com.watabou.utils.Point;
+import com.watabou.utils.Rect;
 
 public class StatuesEntranceRoom extends StatuesRoom {
 
@@ -47,14 +49,32 @@ public class StatuesEntranceRoom extends StatuesRoom {
 
 		int entrance = -1;
 		if (width() >= 11 || height() >= 11){
-			entrance = level.pointToCell(center());
+			Point c = center();
+			entrance = level.pointToCell(c);
 			for (int i : PathFinder.NEIGHBOURS8){
-				if (level.map[entrance + i] == Terrain.STATUE ){
-					Painter.set(level, entrance + i, Terrain.STATUE_SP);
-				} else {
-					Painter.set(level, entrance + i, Terrain.EMPTY_SP);
+				if (level.map[entrance + i] != Terrain.STATUE ){
+					Painter.set(level, entrance + i, Terrain.EMPTY_DECO);
 				}
 			}
+			Carpet carpet = new Carpet();
+			Rect carpetRect = new Rect(c.x-1, c.y-1, c.x+1, c.y+1);
+			if (width()%2 == 0){
+				if (c.x < (left+right)/2f){
+					carpetRect.right++;
+				} else {
+					carpetRect.left--;
+				}
+			}
+			if (height()%2 == 0){
+				if (c.y < (top+bottom)/2f){
+					carpetRect.bottom++;
+				} else {
+					carpetRect.top--;
+				}
+			}
+			carpet.setRect(carpetRect.left, carpetRect.top, carpetRect.width()+1, carpetRect.height()+1);
+			carpet.overrideTile(entrance, level, Carpet.CITY_ENTRANCE);
+			level.customTiles.add(0, carpet); //so other carpets are on top of it
 		} else {
 			//already have an entrance placed in this case
 			for ( Point p : getPoints()){
@@ -65,7 +85,7 @@ public class StatuesEntranceRoom extends StatuesRoom {
 			}
 		}
 
-		Painter.set( level, entrance, Terrain.ENTRANCE_SP );
+		Painter.set( level, entrance, Terrain.ENTRANCE );
 		level.transitions.add(new LevelTransition(level, entrance, LevelTransition.Type.REGULAR_ENTRANCE));
 
 	}
--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/exit/StatuesExitRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/exit/StatuesExitRoom.java
@@ -26,8 +26,10 @@ import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.shatteredpixel.shatteredpixeldungeon.levels.features.LevelTransition;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.StatuesRoom;
+import com.shatteredpixel.shatteredpixeldungeon.tiles.custom.Carpet;
 import com.watabou.utils.PathFinder;
 import com.watabou.utils.Point;
+import com.watabou.utils.Rect;
 
 public class StatuesExitRoom extends StatuesRoom {
 
@@ -47,14 +49,32 @@ public class StatuesExitRoom extends StatuesRoom {
 
 		int exit = -1;
 		if (width() >= 11 || height() >= 11){
-			exit = level.pointToCell(center());
+			Point c = center();
+			exit = level.pointToCell(c);
 			for (int i : PathFinder.NEIGHBOURS8){
-				if (level.map[exit + i] == Terrain.STATUE ){
-					Painter.set(level, exit + i, Terrain.STATUE_SP);
-				} else {
-					Painter.set(level, exit + i, Terrain.EMPTY_SP);
+				if (level.map[exit + i] != Terrain.STATUE ){
+					Painter.set(level, exit + i, Terrain.EMPTY_DECO);
 				}
 			}
+			Carpet carpet = new Carpet();
+			Rect carpetRect = new Rect(c.x-1, c.y-1, c.x+1, c.y+1);
+			if (width()%2 == 0){
+				if (c.x < (left+right)/2f){
+					carpetRect.right++;
+				} else {
+					carpetRect.left--;
+				}
+			}
+			if (height()%2 == 0){
+				if (c.y < (top+bottom)/2f){
+					carpetRect.bottom++;
+				} else {
+					carpetRect.top--;
+				}
+			}
+			carpet.setRect(carpetRect.left, carpetRect.top, carpetRect.width()+1, carpetRect.height()+1);
+			carpet.overrideTile(exit, level, Carpet.SKIP);
+			level.customTiles.add(0, carpet); //so other carpets are on top of it
 		} else {
 			//already have an exit placed in this case
 			for ( Point p : getPoints()){
