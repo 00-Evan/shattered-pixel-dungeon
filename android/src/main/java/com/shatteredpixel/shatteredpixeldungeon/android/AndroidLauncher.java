8ade98e22d581a8879e259804e52359c390b5270
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-01 12:38:08 -0400

    v4.0.0: improved news and updates handling for 3rd party Android stores

--- android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
+++ android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
@@ -91,10 +91,33 @@ public class AndroidLauncher extends AndroidApplication {
 			}
 
 			Gdx.app = this;
-			if (UpdateImpl.supportsUpdates()) {
+
+			//we want to try and ID if we were installed by a 3rd party appstore.
+			// If so we vary update and news checking a bit.
+			String installer;
+			try {
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+					installer = getPackageManager().getInstallSourceInfo(getPackageName()).getInstallingPackageName();
+				} else {
+					installer = getPackageManager().getInstallerPackageName(getPackageName());
+				}
+				if (installer == null) installer = "???";
+			} catch (Exception e) {
+				installer = "???";
+			}
+
+			//if we were installed by a known 3rd party appstore that auto-updates, disable update checking
+			if (UpdateImpl.supportsUpdates()
+					&& !installer.contains("org.fdroid") && !installer.contains("dev.imranr.obtanium")
+					&& !installer.contains("com.looker.droidify") && !installer.contains("com.uptodown")) {
 				Updates.service = UpdateImpl.getUpdateService();
 			}
+
+			//F-Droid specifically considers auto news checking to be an 'anti-feature', so default it to false
 			if (NewsImpl.supportsNews()) {
+				if (installer.contains("org.fdroid") || installer.contains("com.looker.droidify")){
+					SPDSettings.newsDefault = false;
+				}
 				News.service = NewsImpl.getNewsService();
 			}
 
