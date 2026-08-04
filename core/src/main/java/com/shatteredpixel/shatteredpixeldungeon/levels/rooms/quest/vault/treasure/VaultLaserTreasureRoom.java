fbdb4976be821f2b8f8c896b2bcbc32739e54f37
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-03 20:11:07 -0400

    v4.0.0: fixed a treasure rooms dropping consumable twice not a token

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/quest/vault/treasure/VaultLaserTreasureRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/quest/vault/treasure/VaultLaserTreasureRoom.java
@@ -24,6 +24,7 @@ package com.shatteredpixel.shatteredpixeldungeon.levels.rooms.quest.vault.treasu
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.VaultLaser;
 import com.shatteredpixel.shatteredpixeldungeon.items.Heap;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.quest.DwarfToken;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.shatteredpixel.shatteredpixeldungeon.levels.VaultLevel;
@@ -160,7 +161,7 @@ public class VaultLaserTreasureRoom extends VaultTreasureRoom {
 		do {
 			treasurePos = level.pointToCell(Random.element(itemPlace.getPoints()));
 		} while (level.heaps.get(treasurePos) != null);
-		level.drop(treasureItem, treasurePos);
+		level.drop(new DwarfToken(), treasurePos);
 
 	}
 }
