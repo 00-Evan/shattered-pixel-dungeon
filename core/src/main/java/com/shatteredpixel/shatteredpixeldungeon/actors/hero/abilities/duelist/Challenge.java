ec8cabd0078f07c2816abb29a7812ad56b7302f3
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-09 19:14:18 -0400

    v4.0.0: swapped the energy costs of challenge and feint abilities

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/duelist/Challenge.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/duelist/Challenge.java
@@ -55,7 +55,7 @@ import com.watabou.utils.PathFinder;
 public class Challenge extends ArmorAbility {
 
 	{
-		baseChargeUse = 35;
+		baseChargeUse = 50;
 	}
 
 	@Override
--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/duelist/Feint.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/duelist/Feint.java
@@ -56,7 +56,7 @@ import com.watabou.utils.Callback;
 public class Feint extends ArmorAbility {
 
 	{
-		baseChargeUse = 50;
+		baseChargeUse = 35;
 	}
 
 	@Override
