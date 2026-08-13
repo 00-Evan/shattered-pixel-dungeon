3464398afd563cc48b5fe1aea4fc806bc9f64e76
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-12 20:35:19 -0400

    v4.0.0: fixed major issues with Wall Obblusion Tilemap's code structure

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/tiles/WallOcclusionTilemap.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/tiles/WallOcclusionTilemap.java
@@ -24,11 +24,8 @@ package com.shatteredpixel.shatteredpixeldungeon.tiles;
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
-import com.watabou.noosa.TextureFilm;
-import com.watabou.noosa.Tilemap;
-import com.watabou.utils.PathFinder;
 
-public class WallOcclusionTilemap extends Tilemap {
+public class WallOcclusionTilemap extends DungeonTilemap {
 
 	public static final int SIZE = 16;
 
@@ -50,71 +47,42 @@ public class WallOcclusionTilemap extends Tilemap {
 	private static final int WALL_RIGHT_BOTH = 32; //both above and below
 
 	public WallOcclusionTilemap() {
-		super(Assets.Environment.OCCLUSION_SHADOWS, new TextureFilm( Assets.Environment.OCCLUSION_SHADOWS, SIZE, SIZE ) );
-		map( new int[Dungeon.level.length()], Dungeon.level.width());
+		super(Assets.Environment.OCCLUSION_SHADOWS);
+		map( Dungeon.level.map, Dungeon.level.width());
 	}
 
-	@Override
-	public synchronized void updateMap() {
-		for (int cell = 0; cell < data.length; cell++) {
-			//force all top/bottom row, and none-discoverable cells to cleared
-			if (!Dungeon.level.insideMap(cell)
-					|| !Dungeon.level.discoverable[cell]){
-				data[cell] = CLEARED;
-			} else {
-				data[cell] = getTileVisual(cell);
-			}
+	protected int getTileVisual( int pos, int tile, boolean flat ){
+		if (!Dungeon.level.insideMap(pos) || !Dungeon.level.discoverable[pos]){
+			return CLEARED;
 		}
 
-		super.updateMap();
-	}
-
-	@Override
-	public synchronized void updateMapCell(int cell) {
-		//update in a 3x3 grid to account for neighbours which might also be affected
-		if (Dungeon.level.insideMap(cell)) {
-			for (int i : PathFinder.NEIGHBOURS9) {
-				data[cell + i] = getTileVisual(cell + i);
-			}
-			super.updateMapCell(cell - mapWidth - 1);
-			super.updateMapCell(cell + mapWidth + 1);
-
-		//unless we're at the level's edge, then just do the one tile.
-		} else {
-			data[cell] = CLEARED;
-			super.updateMapCell(cell);
-			return;
-		}
-	}
-
-	private int getTileVisual( int cell ){
 		int curr = EMPTY;
-		if (door(cell)){
+		if (DungeonTileSheet.doorTile(tile)){
 
-			if (wall(cell-1) && wall(cell+1)){
+			if (wall(pos-1) && wall(pos+1)){
 				curr = DOOR_HORIZ;
 			} else {
 				curr = DOOR_VERT;
 			}
 
-		} else if (!wall(cell) && !Dungeon.level.pit[cell]) {
+		} else if (!DungeonTileSheet.wallStitcheable(tile) && !Dungeon.level.pit[pos]) {
 
 			//branch for above wall
 			//we specifically skip this for alchemy pots to preserve their passthrough to water layer
-			if (wall(cell-mapWidth) && Dungeon.level.map[cell] != Terrain.ALCHEMY) {
+			if (wall(pos-mapWidth) && map[pos] != Terrain.ALCHEMY) {
 				curr += WALL_ABOVE;
 
 				//left
-				if (wall(cell-1)){
+				if (wall(pos-1)){
 					curr += WALL_LEFT;
-				} else if (wall(cell+mapWidth-1)){
+				} else if (wall(pos+mapWidth-1)){
 					curr += WALL_LEFT_BELOW;
 				}
 
 				//right
-				if (wall(cell+1)){
+				if (wall(pos+1)){
 					curr += WALL_RIGHT;
-				} else if (wall(cell+mapWidth+1)){
+				} else if (wall(pos+mapWidth+1)){
 					curr += WALL_RIGHT_BELOW;
 				}
 
@@ -122,28 +90,28 @@ public class WallOcclusionTilemap extends Tilemap {
 			} else {
 
 				//left
-				if (wall(cell - 1)) {
+				if (wall(pos - 1)) {
 					curr += WALL_LEFT;
-				} else if (wall(cell + mapWidth - 1)) {
-					if (wall(cell - mapWidth - 1)) {
+				} else if (wall(pos + mapWidth - 1)) {
+					if (wall(pos - mapWidth - 1)) {
 						curr += WALL_LEFT_BOTH;
 					} else {
 						curr += WALL_LEFT_BELOW;
 					}
-				} else if (wall(cell - mapWidth - 1)) {
+				} else if (wall(pos - mapWidth - 1)) {
 					curr += WALL_LEFT_ABOVE;
 				}
 
 				//right
-				if (wall(cell + 1)) {
+				if (wall(pos + 1)) {
 					curr += WALL_RIGHT;
-				} else if (wall(cell + mapWidth + 1)) {
-					if (wall(cell - mapWidth + 1)) {
+				} else if (wall(pos + mapWidth + 1)) {
+					if (wall(pos - mapWidth + 1)) {
 						curr += WALL_RIGHT_BOTH;
 					} else {
 						curr += WALL_RIGHT_BELOW;
 					}
-				} else if (wall(cell - mapWidth + 1)) {
+				} else if (wall(pos - mapWidth + 1)) {
 					curr += WALL_RIGHT_ABOVE;
 				}
 
@@ -156,10 +124,10 @@ public class WallOcclusionTilemap extends Tilemap {
 	}
 
 	private boolean wall(int cell) {
-		return DungeonTileSheet.wallStitcheable(Dungeon.level.map[cell]);
+		return DungeonTileSheet.wallStitcheable(map[cell]);
 	}
 
 	private boolean door(int cell) {
-		return DungeonTileSheet.doorTile(Dungeon.level.map[cell]);
+		return DungeonTileSheet.doorTile(map[cell]);
 	}
 }
