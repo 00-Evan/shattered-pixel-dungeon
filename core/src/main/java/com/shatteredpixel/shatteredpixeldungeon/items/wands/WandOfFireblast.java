52ed9fe58bfb9ac93de109de7c7c03587cb2fccd
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-23 17:45:42 -0400

    v4.0.0: fixed fireblast applying paralysis to downed ghouls

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfFireblast.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfFireblast.java
@@ -137,7 +137,7 @@ public class WandOfFireblast extends DamageWand {
 		for ( Char ch : affectedChars ){
 			wandProc(ch, chargesPerCast());
 			ch.damage(damageRoll(), this);
-			if (ch.isAlive()) {
+			if (ch.isActive()) {
 				Buff.affect(ch, Burning.class).reignite(ch);
 				switch (chargesPerCast()) {
 					case 1:
