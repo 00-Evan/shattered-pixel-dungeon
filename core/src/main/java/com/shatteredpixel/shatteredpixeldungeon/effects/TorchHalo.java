6870505b8be4af51fc3d5ecc84108c1eb76613c4
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-23 17:51:38 -0400

    v4.0.0: fixed light halo not account for scaled sprites

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/TorchHalo.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/TorchHalo.java
@@ -57,7 +57,7 @@ public class TorchHalo extends Halo {
 			am = phase * brightness;
 		}
 		
-		point( target.x + target.width / 2, target.y + target.height / 2 );
+		point( target.x + target.width() / 2, target.y + target.height() / 2 );
 	}
 	
 	@Override
