5d7250010b8c0f2f4cd26edcd68bd4667d88c942
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-24 15:44:03 -0400

    v4.0.0: fixed errors with thrown weapon durability on wndUpgrade

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndUpgrade.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndUpgrade.java
@@ -262,18 +262,12 @@ public class WndUpgrade extends Window {
 
 		//durability
 		if (toUpgrade instanceof MissileWeapon){
-			//missile weapons are always IDed currently, so we always use true level
 			int uses1, uses2;
-			if (toUpgrade.levelKnown) {
-				uses1 = (int) Math.ceil(100f / ((MissileWeapon) toUpgrade).durabilityPerUse(toUpgrade.level()));
-				uses2 = (int) Math.ceil(100f / ((MissileWeapon) toUpgrade).durabilityPerUse(toUpgrade.level()+1));
-			} else {
-				uses1 = (int) Math.ceil(100f / ((MissileWeapon) toUpgrade).durabilityPerUse(0));
-				uses2 = (int) Math.ceil(100f / ((MissileWeapon) toUpgrade).durabilityPerUse(1));
-			}
+			uses1 = (int) Math.ceil(100f / ((MissileWeapon) toUpgrade).durabilityPerUse(levelFrom));
+			uses2 = (int) Math.ceil(100f / ((MissileWeapon) toUpgrade).durabilityPerUse(levelTo));
 			bottom = fillFields(Messages.get(this, "durability"),
-					uses1 >= 100 ? "∞" : Integer.toString(uses1),
-					uses2 >= 100 ? "∞" : Integer.toString(uses2),
+					uses1 > 100 ? "∞" : Integer.toString(uses1),
+					uses2 > 100 ? "∞" : Integer.toString(uses2),
 					bottom);
 
 			bottom = fillFields(Messages.get(this, "quantity"),
