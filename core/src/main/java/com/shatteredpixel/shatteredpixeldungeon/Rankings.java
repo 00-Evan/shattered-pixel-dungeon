daa1b487ab1057530caae9908794daeba6997f54
Evan Debenham <Evan@ShatteredPixel.com>
2026-09-02 13:23:30 -0400

    v4.0.0: fixed deaths in the vault using vault inventory for rankings

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Rankings.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Rankings.java
@@ -31,6 +31,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.bags.Bag;
 import com.shatteredpixel.shatteredpixeldungeon.items.potions.Potion;
 import com.shatteredpixel.shatteredpixeldungeon.items.quest.CorpseDust;
+import com.shatteredpixel.shatteredpixeldungeon.items.quest.EscapeCrystal;
 import com.shatteredpixel.shatteredpixeldungeon.items.rings.Ring;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.Scroll;
 import com.shatteredpixel.shatteredpixeldungeon.items.trinkets.Trinket;
@@ -92,6 +93,14 @@ public enum Rankings {
 			rec.version = "v" + m.group();
 		} else {
 			rec.version = "";
+		}
+
+		EscapeCrystal crystal = Dungeon.hero.belongings.getItem(EscapeCrystal.class);
+		EscapeCrystal tempStore = new EscapeCrystal(); //yes we just use a second crystal to store current belongings lmao
+		if (crystal != null){
+			crystal.detachAll(Dungeon.hero.belongings.backpack);
+			tempStore.storeHeroBelongings(Dungeon.hero);
+			crystal.restoreHeroBelongings(Dungeon.hero, null);
 		}
 
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ROOT);
@@ -163,6 +172,10 @@ public enum Rankings {
 		Badges.validateGamesPlayed();
 		
 		save();
+
+		if (crystal != null){
+			tempStore.restoreHeroBelongings(Dungeon.hero, null);
+		}
 	}
 
 	private int score( boolean win ) {
@@ -177,6 +190,15 @@ public enum Rankings {
 			Statistics.progressScore = Math.min(Statistics.progressScore, 50_000);
 
 			if (Statistics.heldItemValue == 0) {
+				EscapeCrystal crystal = Dungeon.hero.belongings.getItem(EscapeCrystal.class);
+				if (crystal != null && crystal.storedItems != null){
+					Belongings stored = new Belongings(Dungeon.hero);
+					stored.restoreFromBundle(crystal.storedItems.getBundle(EscapeCrystal.BELONGINGS));
+
+					for (Item i : stored) {
+
+					}
+				}
 				for (Item i : Dungeon.hero.belongings) {
 					Statistics.heldItemValue += i.value();
 					if (i instanceof CorpseDust && Statistics.deepestFloor >= 10){
--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
@@ -316,7 +316,9 @@ public class EscapeCrystal extends Item {
 
 	public void restoreHeroBelongings( Hero hero, Item preserve ){
 		//we detach the item being preserved first, to cancel any equip-based buffs (e.g. wand charging)
-		preserve.detachAll(hero.belongings.backpack);
+		if (preserve != null) {
+			preserve.detachAll(hero.belongings.backpack);
+		}
 
 		hero.belongings.clear();
 
